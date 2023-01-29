faf9ffb59cd1f1126cc80d99115e50bf8ba2a30b
ReykCS <reyk-carstens@web.de>
2023-01-29 18:43:40 +0100

    added more params for webapp | removed accidental merge conflict relicts

--- training/scripts/train_agent.py
+++ training/scripts/train_agent.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python
-<<<<<<< HEAD
 import sys, rospy, time
-=======
-from typing import Type, Union
-
-import os, sys, rospy, time
 
 from std_msgs.msg import Empty
-
-from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv
-from stable_baselines3.common.callbacks import (
-    EvalCallback,
-    StopTrainingOnRewardThreshold,
-)
-from stable_baselines3.common.policies import ActorCriticPolicy, BasePolicy
->>>>>>> dev
 
 from rosnav.model.agent_factory import AgentFactory
 from tools.argsparser import parse_training_args
@@ -61,6 +47,8 @@ def main():
     eval_cb = init_callbacks(config, train_env, eval_env, PATHS)
     model = get_ppo_instance(config, train_env, PATHS, AgentFactory)
 
+    rospy.on_shutdown(model.env.close())
+    
     # start training
     start = time.time()
     try:
@@ -72,22 +60,10 @@ def main():
     except KeyboardInterrupt:
         print("KeyboardInterrupt..")
 
-<<<<<<< HEAD
-    rospy.on_shutdown(model.env.close())
-    print(f"Time passed: {time.time()-start}s. \n Training script will be terminated..")
-=======
-    model.learn(
-        total_timesteps=n_timesteps,
-        callback=eval_cb,
-        reset_num_timesteps=True,
-    )
 
-    # update the timesteps the model has trained in total
-    # update_total_timesteps_json(n_timesteps, PATHS)
+    print(f"Time passed: {time.time()-start}s. \n Training script will be terminated..")
 
     model.env.close()
-    print(f"Time passed: {time.time()-start}s")
-    print("Training script will be terminated")
 
     publisher = rospy.Publisher("training_finished", Empty, queue_size=10)
 
@@ -95,7 +71,6 @@ def main():
         publisher.publish(Empty())
         rospy.sleep(0.1)
 
->>>>>>> dev
     sys.exit()
 
 
