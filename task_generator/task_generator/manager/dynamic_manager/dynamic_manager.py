111ce6975daae0e9dfa469b1bfa7794130395be4
TheZomb <osmaevhalid@outlook.com>
2023-10-05 13:23:51 +0200

    extends dynamic manager to full interface

--- task_generator/task_generator/manager/dynamic_manager/dynamic_manager.py
+++ task_generator/task_generator/manager/dynamic_manager/dynamic_manager.py
@@ -15,3 +15,26 @@ class DynamicManager:
     
     def dynamic_actor_poses_callback(self, actors):
         ...
+
+    def create_obstacle(self, dynamic, i, map_manager, forbidden_zones):
+        ...
+
+    def spawn_obstacles(self, obstacles, type, yaml, interaction_radius):
+        ...
+
+    def spawn_dynamic_obstacles(self, peds, type, yaml):
+        ...
+
+    def spawn_map_obstacles(self):
+        ...
+
+    def spawn_dynamic_scenario_obstacles(self, peds):
+        ...
+
+    def spawn_scenario_obstacles(self, obstacles, interaction_radius):
+        ...
+
+    def remove_interactive_obstacles(self):
+        ...
+
+    
