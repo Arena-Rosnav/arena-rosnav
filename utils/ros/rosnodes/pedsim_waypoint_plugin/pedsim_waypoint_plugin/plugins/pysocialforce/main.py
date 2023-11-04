6f429950aff0bc46c0db48beb00c459c4d7d1628
MaximilianSchreff <schreffmax@gmail.com>
2023-11-04 14:31:39 +0100

    Added buggy random group assignment

--- utils/ros/rosnodes/pedsim_waypoint_plugin/pedsim_waypoint_plugin/plugins/pysocialforce/main.py
+++ utils/ros/rosnodes/pedsim_waypoint_plugin/pedsim_waypoint_plugin/plugins/pysocialforce/main.py
@@ -4,6 +4,7 @@ import pedsim_msgs.msg
 import numpy as np
 from pathlib import Path
 from typing import Dict, List, Optional, Type, TypeVar
+import random
 
 import sys
 sys.path.append(str(Path(__file__).resolve().parent))
@@ -15,16 +16,40 @@ class Plugin_PySocialForce(WaypointPlugin):
     def __init__(self):
         self.first_call = True
         self.simulator = None
+        self.groups = list()
+
+    def assign_groups(self,
+                   agents: dict,
+                   groups: List[pedsim_msgs.msg.AgentGroup],
+                   ) -> list:
+        
+        # assign new agents to groups
+        for agent_id, idx in agents.items():
+            if agent_id in self.groups:
+                # agent already went through assignment
+                pass
+
+            assigned_group = random.randint(0, len(self.groups))
+            if assigned_group == len(self.groups):
+                self.groups.append([agent_id])
+            else:
+                self.groups[assigned_group].append(agent_id)
+
+        # form group list
+        return [[agents[id] for id in group] for group in self.groups]
 
     @classmethod
     def get_state_data(cls, 
                        agents: List[pedsim_msgs.msg.AgentState], 
                        groups: List[pedsim_msgs.msg.AgentGroup],
                        reset_velocity: bool = False
-                       ) -> (np.ndarray, List):
+                       ) -> np.ndarray:
         
+        idx_assignment = dict()
         state_data = list()
-        for agent in agents:
+        for idx in range(len(agents)):
+            agent = agents[idx]
+
             p_x = agent.pose.position.x
             p_y = agent.pose.position.y
             v_x = agent.twist.linear.x if not reset_velocity else 1.0
@@ -32,12 +57,10 @@ class Plugin_PySocialForce(WaypointPlugin):
             d_x = agent.destination.x
             d_y = agent.destination.y
             state_data.append([p_x, p_y, v_x, v_y, d_x, d_y])
+            idx_assignment[agent.id] = idx
         state = np.array(state_data)
-
-        # hard-coded
-        groups = [[0, 2], [3, 4, 5]]
         
-        return state, groups
+        return state, idx_assignment
     
     @classmethod
     def map_force_to_feedback(cls,
@@ -60,7 +83,9 @@ class Plugin_PySocialForce(WaypointPlugin):
         if len(data.agents) < 1:
             return list()
         
-        state, groups = self.get_state_data(data.agents, data.groups, reset_velocity=self.first_call)
+        state, agent_idx = self.get_state_data(data.agents, data.groups, reset_velocity=self.first_call)
+        groups = self.assign_groups(agent_idx, data.groups)
+        print(groups)
 
         if self.first_call:
             # instantiate sim
