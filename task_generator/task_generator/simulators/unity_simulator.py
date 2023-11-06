f972ffc2ff6d1cdacd6103679b0a5a4643f00a07
TheZomb <osmaevhalid@outlook.com>
2023-11-07 00:35:04 +0100

    extends example services

--- task_generator/task_generator/simulators/unity_simulator.py
+++ task_generator/task_generator/simulators/unity_simulator.py
@@ -1,6 +1,4 @@
 import rospy
-import os
-import requests
 
 from task_generator.simulators.simulator_factory import SimulatorFactory
 from task_generator.utils import rosparam_get
@@ -10,8 +8,10 @@ from task_generator.simulators.base_simulator import BaseSimulator
 
 from task_generator.shared import EntityProps, ModelType, Robot, ObstacleProps, PositionOrientation
 
+from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
+
 from std_msgs.msg import Empty
-from std_srvs.srv import Empty
+from std_srvs.srv import Empty, EmptyRequest
 
 T = Constants.WAIT_FOR_SERVICE_TIMEOUT
 
@@ -27,45 +27,45 @@ class UnitySimulator(BaseSimulator):
 
         rospy.loginfo("Waiting for Unity services...")
 
-        rospy.wait_for_service("unity/spawn_model", timeout=T)
+        rospy.wait_for_service(self._namespace(
+            "unity", "spawn_model"), timeout=T)
+        rospy.wait_for_service(self._namespace(
+            "unity", "delete_model"), timeout=T)
+        rospy.wait_for_service(self._namespace(
+            "unity", "set_model_state"), timeout=T)
 
-        self._spawn_model[ModelType.URDF] = rospy.ServiceProxy(
-            "unity/spawn_model", Empty
+        # TODO: Proper Message Types
+        self._spawn_model = rospy.ServiceProxy(
+            self._namespace("unity", "spawn_model"), Empty
         )
-        self._spawn_model[ModelType.SDF] = rospy.ServiceProxy(
-            "unity/spawn_model", Empty
+        self._remove_model_srv = rospy.ServiceProxy(
+            self._namespace("unity", "delete_model"), Empty
+        )
+        self._move_model_srv = rospy.ServiceProxy(
+            self._namespace("unity", "set_model_state"), Empty, persistent=True
+        )
+        self._goal_pub = rospy.Publisher(
+            self._namespace("unity", "set_goal"), PoseStamped, queue_size=1, latch=True
         )
 
         rospy.loginfo("...Unity services now available.")
 
     def before_reset_task(self):
-        """
-        Is executed each time before the task is reseted. This is useful in
-        order to pause the simulation and physics.
-        """
-        raise NotImplementedError()
+        pass
 
     def after_reset_task(self):
-        """
-        Is executed after the task is reseted. This is useful to unpause the
-        simulation and physics.
-        """
-        raise NotImplementedError()
+        pass
 
-    # OBSTACLE
-    def spawn_obstacle(self, obstacle: ObstacleProps) -> bool:
-        raise NotImplementedError()
-    
     def spawn_entity(self, entity: EntityProps) -> bool:
-        self._spawn_model("This is only an example call of the function")
-        raise NotImplementedError()
-        
-    # ROBOT
+        req = EmptyRequest()
+        self._spawn_model(req)
+
     def spawn_robot(self, robot: Robot) -> str:
         """
         Spawn a robot in the simulator.
         """
-        raise NotImplementedError()
+        req = EmptyRequest()
+        self._spawn_model(req)
 
     def move_entity(self, name: str, pos: PositionOrientation):
         """
@@ -74,4 +74,7 @@ class UnitySimulator(BaseSimulator):
         raise NotImplementedError()
 
     def delete_entity(self, name: str) -> bool:
+        raise NotImplementedError()
+
+    def _publish_goal(self, goal):
         raise NotImplementedError()
