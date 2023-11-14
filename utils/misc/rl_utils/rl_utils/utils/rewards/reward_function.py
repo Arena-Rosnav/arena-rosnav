c564e00c12abea9e0b333c19d982d6d1f125710e
tuananhroman <anh.maxiking@hotmail.de>
2023-11-14 20:01:41 +0100

    new reward function class

--- utils/misc/rl_utils/rl_utils/utils/rewards/reward_function.py
+++ utils/misc/rl_utils/rl_utils/utils/rewards/reward_function.py
@@ -1,31 +1,88 @@
-from tools.general import load_rew_fnc
+from typing import Any, Callable, Dict, List
 
-import rospy
 import numpy as np
+import rospy
 
 from .constants import REWARD_CONSTANTS
+from .utils import load_rew_fnc
 
 
 class RewardFunction:
-    def __init__(self, holonomic, robot_radius, goal_radius, safe_dist):
+    def __init__(
+        self,
+        rew_func_name: str,
+        holonomic: bool,
+        robot_radius: float,
+        goal_radius: float,
+        safe_dist: float,
+    ):
+        self._rew_func_name = rew_func_name
         self._holonomic = holonomic
         self._robot_radius = robot_radius
         self._safe_dist = safe_dist
         self._goal_radius = goal_radius
 
-        self._last_goal_dist: float = None
-        self._last_dist_to_path: float = None
-        self._last_action: np.ndarray = None
         self._curr_dist_to_path: float = None
         self._safe_dist_breached: bool = None
-        self._kdtree: np.ndarray = None
 
         self._curr_reward = 0
         self._info = {}
 
+        self._reward_units: List[
+            Callable[[Dict[str, Any]], None]
+        ] = RewardFunction._setup_reward_function(self, self._rew_func_name)
+
+    @staticmethod
+    def _setup_reward_function(
+        rew_fnc_cls: "RewardFunction", rew_fnc_name: str
+    ) -> List[Callable[[Dict[str, Any]], None]]:
+        import rl_utils.utils.rewards as rew_pkg
+
+        rew_fnc_dict = load_rew_fnc(rew_fnc_name)
+        return [
+            rew_pkg.RewardUnitFactory.instantiate(unit_name)(
+                reward_function=rew_fnc_cls, **kwargs
+            )
+            for unit_name, kwargs in rew_fnc_dict.items()
+        ]
+
+    def add_reward(self, value: float):
+        self._curr_reward += value
+
+    def add_info(self, info: dict):
+        self._info.update(info)
+
+    def _reset(self):
+        self._curr_reward = 0
+        self._info = {}
+
+    def reset(self):
+        self.goal_radius = rospy.get_param("/goal_radius", 0.3)
+        self._curr_dist_to_path = None
+
+        for reward_unit in self._reward_units:
+            reward_unit.reset()
+
+    def calculate_reward(self, *args, **kwargs):
+        self._reset()
+        self.set_safe_dist_breached(kwargs["laser_scan"])
+
+        for reward_unit in self._reward_units:
+            if self.safe_dist_breached and not reward_unit.on_safe_dist_violation:
+                continue
+            reward_unit(**kwargs)
+
+    def get_reward(self, *args, **kwargs):
+        self.calculate_reward(**kwargs)
+        return self._curr_reward, self._info
+
+    @property
+    def holonomic(self) -> float:
+        return self._holonomic
+
     @property
     def robot_radius(self) -> float:
-        return self.robot_radius
+        return self._robot_radius
 
     @property
     def goal_radius(self) -> float:
@@ -43,6 +100,10 @@ class RewardFunction:
     def curr_dist_to_path(self) -> float:
         return self._curr_dist_to_path
 
+    @curr_dist_to_path.setter
+    def curr_dist_to_path(self, value: float) -> None:
+        self._curr_dist_to_path = value
+
     @property
     def safe_dist_breached(self) -> bool:
         return self._safe_dist_breached
@@ -50,24 +111,10 @@ class RewardFunction:
     def set_safe_dist_breached(self, laser_scan: np.ndarray) -> None:
         self._safe_dist_breached = laser_scan.min() <= self._safe_dist
 
-    def add_reward(self, value: float):
-        self._curr_reward += value
-
-    def add_info(self, info: dict):
-        self._info.update(info)
-
-    def _reset(self):
-        self._curr_reward = 0
-        self._info = {}
-
-    def reset(self):
-        self.goal_radius = rospy.get_param("/goal_radius", 0.3)
-
-        self._last_goal_dist = None
-        self._last_dist_to_path = None
-        self._last_action = None
-        self._kdtree = None
-        self._curr_dist_to_path = None
-
-    def calculate_reward(self, *args, **kwargs):
-        self.set_safe_dist_breached(kwargs["laser_scan"])
+    def __repr__(self) -> str:
+        format_string = self.__class__.__name__ + "("
+        for t in self._reward_units:
+            format_string += "\n"
+            format_string += f"    {t}"
+        format_string += "\n)"
+        return format_string
