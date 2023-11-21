91152e82021037fc3a909da053b025833028511a
diego <d.diaz@campus.tu-berlin.de>
2023-11-21 16:50:11 +0100

    requested changes by vova after 1st review

--- utils/ros/rosnodes/pedsim_waypoint_plugin/pedsim_waypoint_plugin/__init__.py
+++ utils/ros/rosnodes/pedsim_waypoint_plugin/pedsim_waypoint_plugin/__init__.py
@@ -2,5 +2,5 @@ from .plugins.spinny import main # noqa
 from .plugins.passthrough import main # noqa
 from .plugins.pysocial1 import main # noqa
 from .plugins.pysocial2 import main # noqa
-#from .plugins.deepsocialforce import main # noqa
+from .plugins.deepsocialforce import main # noqa
 from .plugins.evacuation import main # noqa
