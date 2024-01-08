810337b9aa739d174b96100faff9402d5b936a92
tuananhroman <anh.maxiking@hotmail.de>
2024-01-08 18:23:55 +0100

    dynamic param publisher

--- training/tools/dynamic_parameter.py
+++ training/tools/dynamic_parameter.py
@@ -40,3 +40,38 @@ class DynamicParameter:
         self._sub = rospy.Subscriber(
             f"{self._topic_prefix}{self._key}", message_type, self._callback
         )
+
+
+class DynamicParameterPublisher:
+    """
+    Represents a dynamic parameter that can be updated through a ROS topic.
+    The parameter is updated by publishing to a ROS topic.
+
+    Args:
+        key (Any): The key or name of the parameter to be updated.
+        message_type: The ROS message type used for updating the parameter.
+        topic_prefix (str, optional): The prefix for the ROS topic used for updating the parameter.
+            Defaults to "/update_param/".
+
+    Attributes:
+        _key (Any): The key or name of the parameter to be updated.
+        _topic_prefix (str): The prefix for the ROS topic used for updating the parameter.
+        _pub (rospy.Publisher): The ROS publisher for publishing updates to the parameter.
+    """
+
+    def __init__(self, key: Any, message_type, topic_prefix: str = "/update_param/"):
+        self._key = key
+        self._topic_prefix = topic_prefix
+
+        self._pub = rospy.Publisher(
+            f"{self._topic_prefix}{self._key}", message_type, queue_size=1
+        )
+
+    def __call__(self, value: Any):
+        """
+        Publishes the value to the parameter.
+
+        Args:
+            value (Any): The value to be published.
+        """
+        self._pub.publish(value)
