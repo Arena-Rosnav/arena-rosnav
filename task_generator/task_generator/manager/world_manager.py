fixes rotation for round table

task_generator/task_generator/manager/world_manager.py
@@ -347,7 +347,7 @@ class WorldManager:
                 position=PositionOrientation(
                     x= obst.position.position.x * self.resolution,
                     y= obst.position.position.y * self.resolution,
-                    orientation=obst.position.orientation.z
+                    orientation=obst.position.orientation.z + np.pi/2
                 ),
                 model_name=obst.model_name,
                 extra={}

utils/misc/map_generator/map_generator/rosmap/wrapper.py
@@ -84,22 +84,22 @@ def place_rect(center, rot: float, chair_chance: float):
         pos = [center[0] - 1, center[1] - 0.8]
         pos = rotate_point(pos, np.rad2deg(rot), center)
         chairs.append(Obstacle(model_name="chair", position=Pose(position=Point(
-            x=pos[0], y=pos[1], z=0), orientation=Quaternion(z=rot))))
+            x=pos[0], y=pos[1], z=0), orientation=Quaternion(z=rot - np.pi/2))))
     if np.random.random() <= chair_chance:
         pos = [center[0] - 1, center[1] + 0.8]
         pos = rotate_point(pos, np.rad2deg(rot), center)
         chairs.append(Obstacle(model_name="chair", position=Pose(position=Point(
-            x=pos[0], y=pos[1], z=0), orientation=Quaternion(z=rot))))
+            x=pos[0], y=pos[1], z=0), orientation=Quaternion(z=rot - np.pi/2))))
     if np.random.random() <= chair_chance:
         pos = [center[0] + 1, center[1] - 0.8]
         pos = rotate_point(pos, np.rad2deg(rot), center)
         chairs.append(Obstacle(model_name="chair", position=Pose(position=Point(
-            x=pos[0], y=pos[1], z=0), orientation=Quaternion(z=np.pi + rot))))
+            x=pos[0], y=pos[1], z=0), orientation=Quaternion(z=rot + np.pi/2))))
     if np.random.random() <= chair_chance:
         pos = [center[0] + 1, center[1] + 0.8]
         pos = rotate_point(pos, np.rad2deg(rot), center)
         chairs.append(Obstacle(model_name="chair", position=Pose(position=Point(
-            x=pos[0], y=pos[1], z=0), orientation=Quaternion(z=np.pi + rot))))
+            x=pos[0], y=pos[1], z=0), orientation=Quaternion(z=rot + np.pi/2))))
 
     return chairs
 