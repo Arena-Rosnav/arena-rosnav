d3bf4c7141e8500f145a7f153a6a717d7c80c1a6
Nhat <Nhat>
2024-01-29 00:07:37 +0100

    update installer

--- install.sh
+++ install.sh
@@ -36,6 +36,8 @@ echo "Installing ROS...:"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
+sudo apt-get update
+sudo apt-get install python3-catkin-tools
 sudo apt install -y ros-noetic-desktop-full
 if ! grep -q "source /opt/ros/noetic/setup.bash" ~/.bashrc; then
   echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
