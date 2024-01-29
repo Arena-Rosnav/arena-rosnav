a7f9b4b2dad9020030e05f0b8bf42b0d1b100aaf
Nhat <Nhat>
2024-01-29 01:09:36 +0100

    change

--- install.sh
+++ install.sh
@@ -36,8 +36,6 @@ echo "Installing ROS...:"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt-get update
-sudo apt-get install python3-catkin-tools
 sudo apt install -y ros-noetic-desktop-full
 if ! grep -q "source /opt/ros/noetic/setup.bash" ~/.bashrc; then
   echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
@@ -46,7 +44,7 @@ source ~/.bashrc
 
 # Getting Packages
 echo "Installing Deps...:"
-sudo apt install -y python3 python-is-python3 git python3-rosdep python3-pip python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install -y python3 python-is-python3 git python3-rosdep python3-pip python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-catkin-tools
 
 # Poetry
 echo "Installing Poetry...:"
