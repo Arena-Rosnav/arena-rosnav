48649e44a09ace1c03d18bede6b8b10a0287d3b0
MaximilianSchreff <schreffmax@gmail.com>
2024-02-02 22:21:40 +0100

    Changed Unity timer to use Threads.Event instead of rospy.Rate

--- utils/misc/rl_utils/rl_utils/utils/arena_unity_utils/unity_timer.py
+++ utils/misc/rl_utils/rl_utils/utils/arena_unity_utils/unity_timer.py
@@ -1,3 +1,5 @@
+from threading import Event
+
 from rosgraph_msgs.msg import Clock
 import rospy
 
@@ -18,30 +20,32 @@ class UnityTimer:
                 published to.
         """
         self._current_time = start_time
-        # self._update_duration = update_duration
-        self._update_duration = 3
+        self._update_duration = update_duration
         self._update_offset = rospy.Duration(secs=self._update_duration, nsecs=0)
         self._next_update = self._current_time + self._update_offset
         self._clock_subscrition = rospy.Subscriber(clock_topic, Clock, self._clock_callback)
 
+        self._event = Event()
+        self._waiting = False
+
     def _clock_callback(self, clock_msg: Clock):
         self._current_time = rospy.Time(clock_msg.clock.secs, clock_msg.clock.nsecs)
+
+        if self._waiting and self._current_time >= self._next_update:
+            self._waiting = False
+
+            # update next update time
+            if self._current_time >= self._next_update + self._update_offset:
+                rospy.logwarn(f"Training loop missed rate of {1.0 / self._update_duration} Hz. Missed update at {self._next_update} nsecs and rescheduling next update to {self._current_time + self._update_offset} nsecs")
+                self._next_update = self._current_time + self._update_offset
+            else:
+                self._next_update = self._next_update + self._update_offset
+
+            self._event.set()
+            self._event.clear()
 
     def wait_for_next_update(self):
         """Waits until the unity clock has passed one update interval since the last one.
         """
-        r = rospy.Rate(10 * 1.0 / self._update_duration)
-        
-        rospy.loginfo("*****************Wating for update")
-        
-        while self._current_time < self._next_update and not rospy.is_shutdown():
-            if self._current_time >= self._next_update:
-                # check if loop is too slow
-                if self._current_time >= self._next_update + self._update_offset:
-                    rospy.logwarn(f"Training loop missed rate of {1.0 / self._update_duration}. Missed update {self._next_update} and rescheduling next update to {self._current_time + self._update_offset}")
-
-                self._next_update = self._current_time + self._update_offset
-                return
-            rospy.loginfo("*****************Going back to sleep")
-
-            r.sleep()
+        self._waiting = True
+        self._event.wait(timeout=10)
