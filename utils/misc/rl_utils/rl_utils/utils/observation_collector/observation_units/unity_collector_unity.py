c4ee27cd66b919c56e1db85b39681a4e4528986c
MaximilianSchreff <schreffmax@gmail.com>
2024-02-22 14:32:28 +0100

    Integrated rgbd topics into unity collector unit

--- utils/misc/rl_utils/rl_utils/utils/observation_collector/observation_units/unity_collector_unity.py
+++ utils/misc/rl_utils/rl_utils/utils/observation_collector/observation_units/unity_collector_unity.py
@@ -1,8 +1,12 @@
 from time import sleep
 from typing import Any, Dict
 
+import numpy as np
+
 import rospy
+from cv_bridge import CvBridge
 from unity_msgs.msg import Collision
+from sensor_msgs.msg import Image
 from task_generator.shared import Namespace
 
 from ..constants import OBS_DICT_KEYS, TOPICS, MAX_WAIT, SLEEP
@@ -37,16 +41,22 @@ class UnityCollectorUnit(CollectorUnit):
     _collision: bool
     _ped_safe_dist: bool
     _obs_safe_dist: bool
+    _image_depth: np.ndarray
+    _image_color: np.ndarray
 
     # Subscriptions
     _collision_sub: rospy.Subscriber
     _ped_safe_dist_sub: rospy.Subscriber
     _obs_safe_dist_sub: rospy.Subscriber
+    _image_depth_sub: rospy.Subscriber
+    _image_color_sub: rospy.Subscriber
 
     # Received Flags
     _received_collision: bool
     _received_ped_safe_dist: bool
     _received_obs_safe_dist: bool
+    _received_image_depth: bool
+    _received_image_color: bool
 
     _first_reset: bool
 
@@ -59,6 +69,10 @@ class UnityCollectorUnit(CollectorUnit):
             observation_manager: Observation manager holding this collector unit.
         """
         super().__init__(ns, observation_manager)
+        
+        self._enable_rgbd = rospy.get_param("rgbd/enabled", False)
+        if self._enable_rgbd:
+            self._image_bridge = CvBridge()
 
         self._collision = False
         self._ped_safe_dist = False
@@ -96,6 +110,25 @@ class UnityCollectorUnit(CollectorUnit):
             self._cb_obs_safe_dist,
             tcp_nodelay=True,
         )
+        if self._enable_rgbd:
+            self._image_color_sub = rospy.Subscriber(
+                self._ns(TOPICS.IMAGE_COLOR),
+                Image,
+                self._cb_image_color,
+                tcp_nodelay=True
+            )
+            self._image_depth_sub = rospy.Subscriber(
+                self._ns(TOPICS.IMAGE_DEPTH),
+                Image,
+                self._cb_image_depth,
+                tcp_nodelay=True
+            )
+            
+    def _rgbd_wait_condition(self):
+        return (
+                    not self._enable_rgbd or 
+                    (self._received_image_color and self._received_image_depth)
+                )
 
     def wait(self):
         """
@@ -108,7 +141,8 @@ class UnityCollectorUnit(CollectorUnit):
         for _ in range(int(MAX_WAIT / SLEEP)):
             if (self._received_collision and
                 self._received_ped_safe_dist and
-                self._received_obs_safe_dist):
+                self._received_obs_safe_dist and
+                self._rgbd_wait_condition()):
                 return
 
             sleep(SLEEP)
@@ -170,3 +204,26 @@ class UnityCollectorUnit(CollectorUnit):
         """
         self._received_obs_safe_dist = True
         self._obs_safe_dist = collsion_msg.collision
+
+    def _cb_image_color(self, image_msg: Image):
+        """
+        Callback function for receiving rgb data. Converts image msg to cv Mat to np ndarray.
+
+        Args:
+            image_msg (Image): Received rgb image message.
+        """
+        self._received_image_color = True
+        cv_mat = self._image_bridge.imgmsg_to_cv2(image_msg)
+        self._image_color = np.asarray(cv_mat)
+        # let the channel dim be the first dim and remove a-channel
+        self._image_color = (np.asarray(cv_mat)[:,:,0:3]).transpose((2, 0, 1))
+
+    def _cb_image_depth(self, image_msg: Image):
+        """Callback function for receiving depth data. Converts image msg to cv Mat to np ndarray.
+
+        Args:
+            image_msg (Image): Received depth image message.
+        """
+        self._received_image_depth = True
+        cv_mat = self._image_bridge.imgmsg_to_cv2(image_msg)
+        self._image_depth = np.asarray(cv_mat)
