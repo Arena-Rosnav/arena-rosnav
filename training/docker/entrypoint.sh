d4bf47d82d2cdefe98b0dd7e974a31c46d475b00
tuananhroman <anh.maxiking@hotmail.de>
2024-04-29 18:07:12 +0200

    dockerize

---
+++ training/docker/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Start virtual X server in the background
+# - DISPLAY default is :99, set in dockerfile
+# - Users can override with `-e DISPLAY=` in `docker run` command to avoid
+#   running Xvfb and attach their screen
+if [[ -x "$(command -v Xvfb)" && "$DISPLAY" == ":99" ]]; then
+	echo "Starting Xvfb"
+	Xvfb :99 -screen 0 1600x1200x24+32 &
+fi
+
+# Check if the ROS_DISTRO is passed and use it
+# to source the ROS environment
+if [ -n "${ROS_DISTRO}" ]; then
+	source "/opt/ros/$ROS_DISTRO/setup.bash"
+fi
+
+# Use the LOCAL_USER_ID if passed in at runtime
+if [ -n "${LOCAL_USER_ID}" ]; then
+	echo "Starting with UID : $LOCAL_USER_ID"
+	# modify existing user's id
+	usermod -u $LOCAL_USER_ID user
+	# run as user
+	exec gosu user "$@"
+else
+	exec "$@"
+fi
+
+# Check if the shell is bash or zsh
+if [[ -n "$SHELL" ]]; then
+    shell=$(basename $SHELL)
+
+echo "[ARENA-ROSNAV] Using $shell as the shell."
+
+# Set the workspace variable
+arena_ws="${ARENA_WS:-catkin_ws}"
+arena_root="${ARENA_ROOT:-$HOME}"
+
+cd $arena_root/$arena_root/src/arena/arena-rosnav/scripts
+./train_agent.sh	
---
+++ training/scripts/train_agent.sh
@@ -0,0 +1,45 @@
+#!/bin/bsh
+
+echo "[ARENA-ROSNAV] Starting training shell script..."
+
+# Check if the shell is bash or zsh
+if [[ -n "$SHELL" ]]; then
+    shell=$(basename $SHELL)
+else
+    echo "[ARENA-ROSNAV] Unsupported shell. Please use bash or zsh."
+    exit 1
+fi
+
+# Set the workspace variable
+arena_ws="${ARENA_WS:-catkin_ws}"
+arena_root="${ARENA_ROOT:-$HOME}"
+
+# Set the arena directory
+arena_dir=${arena_root}/${arena_ws}/src/arena/arena-rosnav
+
+echo "[ARENA-ROSNAV] Using workspace at: ${arena_dir}"
+
+# Source ROS
+source /opt/ros/noetic/setup.${shell}
+# Source workspace
+source ${arena_root}/${arena_ws}/devel/setup.${shell}
+echo "[ARENA-ROSNAV] Sourced the workspace."
+
+# Activate the poetry environment
+echo "arena_dir: $arena_dir"
+cd $arena_dir
+poetry shell
+
+echo "[ARENA-ROSNAV] Activated the poetry environment."
+
+# Load config
+source $arena_dir/arena_bringup/configs/training/train_from_sh.cfg
+
+# Start training
+roslaunch arena_bringup start_training.launch \
+    num_envs:=$num_envs \
+    model:=$model \
+    map_folder_name:=$map_folder_name \
+    entity_manager:=$entity_manager \
+    sfm:=$sfm \
+    cfg:=$cfg
