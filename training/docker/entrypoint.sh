358af36c9922fd3f4268a9e6c53cddd37b8825bd
tuananhroman <anh.maxiking@hotmail.de>
2024-04-30 00:52:55 +0200

    update

--- training/docker/entrypoint.sh
+++ training/docker/entrypoint.sh
@@ -5,25 +5,25 @@
 # - Users can override with `-e DISPLAY=` in `docker run` command to avoid
 #   running Xvfb and attach their screen
 if [[ -x "$(command -v Xvfb)" && "$DISPLAY" == ":99" ]]; then
-	echo "Starting Xvfb"
-	Xvfb :99 -screen 0 1600x1200x24+32 &
+    echo "Starting Xvfb"
+    Xvfb :99 -screen 0 1600x1200x24+32 &
 fi
 
 # Check if the ROS_DISTRO is passed and use it
 # to source the ROS environment
 if [ -n "${ROS_DISTRO}" ]; then
-	source "/opt/ros/$ROS_DISTRO/setup.bash"
+    source "/opt/ros/$ROS_DISTRO/setup.bash"
 fi
 
 # Use the LOCAL_USER_ID if passed in at runtime
 if [ -n "${LOCAL_USER_ID}" ]; then
-	echo "Starting with UID : $LOCAL_USER_ID"
-	# modify existing user's id
-	usermod -u $LOCAL_USER_ID user
-	# run as user
-	exec gosu user "$@"
+    echo "Starting with UID : $LOCAL_USER_ID"
+    # modify existing user's id
+    usermod -u $LOCAL_USER_ID user
+    # run as user
+    exec gosu user "$@"
 else
-	exec "$@"
+    exec "$@"
 fi
 
 # Check if the shell is bash or zsh
