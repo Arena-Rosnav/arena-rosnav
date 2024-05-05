0d7c2fbf1c25ef2c00793b760d5c162ea57c95cd
tuananhroman <anh.maxiking@hotmail.de>
2024-05-05 17:21:30 +0200

    update docker

--- training/docker/entrypoint.sh
+++ training/docker/entrypoint.sh
@@ -18,6 +18,9 @@ fi
 # Check if the shell is bash or zsh
 if [[ -n "$SHELL" ]]; then
     shell=$(basename $SHELL)
+else
+    echo "[ARENA-ROSNAV] Unsupported shell. Please use bash or zsh."
+    exit 1
 fi
 
 echo "[ARENA-ROSNAV] Using $shell as the shell."
@@ -25,9 +28,20 @@ echo "[ARENA-ROSNAV] Using $shell as the shell."
 # Set the workspace variable
 arena_ws="${ARENA_WS:-catkin_ws}"
 arena_root="${ARENA_ROOT:-$HOME}"
+arena_dir=${arena_root}/${arena_ws}/src/arena/arena-rosnav
+
+if [[ "$shell" == "bash" ]]; then
+    source /opt/ros/noetic/setup.bash
+else
+    source /opt/ros/noetic/setup.zsh
+fi
+
+# Retrieve the poetry environment path
+cd $arena_dir && POETRY_ENV_PATH=$(poetry env info --path)/bin/python
+# Build the workspace
+cd $arena_root/$arena_ws && catkin_make -DPYTHON_EXECUTABLE=$POETRY_ENV_PATH
 
 cd $arena_root/$arena_ws/src/arena/arena-rosnav/training/scripts
-
 if [[ "$shell" == "bash" ]]; then
     bash train_agent.sh
 else
