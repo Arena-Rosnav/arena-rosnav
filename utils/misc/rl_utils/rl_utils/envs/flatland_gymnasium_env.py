df9a2aee45b489e2bbc2bb4bbc813c93f4068b34
tuananhroman <anh.maxiking@hotmail.de>
2024-05-10 19:45:17 +0200

    fix robot radius retrieval

--- utils/misc/rl_utils/rl_utils/envs/flatland_gymnasium_env.py
+++ utils/misc/rl_utils/rl_utils/envs/flatland_gymnasium_env.py
@@ -125,6 +125,7 @@ class FlatlandEnv(gymnasium.Env):
             bool: True if the initialization is successful, False otherwise.
         """
         self.model_space_encoder = RosnavSpaceManager(
+            simulation_ns=self.ns,
             space_encoder_class=self._agent_description.space_encoder_class,
             observation_spaces=self._agent_description.observation_spaces,
             observation_space_kwargs=self._agent_description.observation_space_kwargs,
