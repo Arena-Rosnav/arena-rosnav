7b7307a4950669cfcee1631e4021554b8e84b5d2
Max <schreffmax@gmail.com>
2024-05-18 10:56:42 +0900

    Removed RGBD part from UnityCollectorUnit
    Now the UnityCollectorUnit consists only of unity-specific observations which are all only used for reward functions.
    I.e. they can be left out in agent testing mode.

--- utils/misc/rl_utils/rl_utils/utils/observation_collector/observation_units/unity_collector_unit.py
+++ utils/misc/rl_utils/rl_utils/utils/observation_collector/observation_units/unity_collector_unit.py
@@ -16,8 +16,7 @@ from .collector_unit import CollectorUnit
 
 class UnityCollectorUnit(CollectorUnit):
     """
-    A class for collecting Arena Unity specific navigation information such as collider data (safe 
-    distances and collisons).
+    A class for collecting Arena Unity specific collider data used only for reward functions.
 
     Attributes:
         _collision (bool): True if robot is currently colliding with something.
@@ -41,22 +40,16 @@ class UnityCollectorUnit(CollectorUnit):
     _collision: bool
     _ped_safe_dist: bool
     _obs_safe_dist: bool
-    _image_depth: np.ndarray
-    _image_color: np.ndarray
 
     # Subscriptions
     _collision_sub: rospy.Subscriber
     _ped_safe_dist_sub: rospy.Subscriber
     _obs_safe_dist_sub: rospy.Subscriber
-    _image_depth_sub: rospy.Subscriber
-    _image_color_sub: rospy.Subscriber
 
     # Received Flags
     _received_collision: bool
     _received_ped_safe_dist: bool
     _received_obs_safe_dist: bool
-    _received_image_depth: bool
-    _received_image_color: bool
 
     _first_reset: bool
 
@@ -70,11 +63,6 @@ class UnityCollectorUnit(CollectorUnit):
         """
         super().__init__(ns, observation_manager)
         
-        self._enable_rgbd = rospy.get_param("rgbd/enabled", False)
-        if self._enable_rgbd:
-            self._image_bridge = CvBridge()
-
-        self._train_mode = rospy.get_param("train_mode", True)
         self._collision = False
         self._ped_safe_dist = False
         self._obs_safe_dist = False
@@ -93,55 +81,23 @@ class UnityCollectorUnit(CollectorUnit):
         """
         Initialize the subscribers for robot state and sensor data.
         """
-        if self._train_mode:
-            # only use these observations in train mode since they
-            # are only used for reward calculation
-            self._collision_sub = rospy.Subscriber(
-                self._ns(TOPICS.COLLISION),
-                Collision,
-                self._cb_collision,
-                tcp_nodelay=True,
-            )
-            self._ped_safe_dist_sub = rospy.Subscriber(
-                self._ns(TOPICS.PED_SAFE_DIST),
-                Collision,
-                self._cb_ped_safe_dist,
-                tcp_nodelay=True,
-            )
-            self._obs_safe_dist_sub = rospy.Subscriber(
-                self._ns(TOPICS.OBS_SAFE_DIST),
-                Collision,
-                self._cb_obs_safe_dist,
-                tcp_nodelay=True,
-            )
-        if self._enable_rgbd:
-            self._image_color_sub = rospy.Subscriber(
-                self._ns(TOPICS.IMAGE_COLOR),
-                Image,
-                self._cb_image_color,
-                tcp_nodelay=True
-            )
-            self._image_depth_sub = rospy.Subscriber(
-                self._ns(TOPICS.IMAGE_DEPTH),
-                Image,
-                self._cb_image_depth,
-                tcp_nodelay=True
-            )
-            
-    def _rgbd_wait_condition(self):
-        return (
-            not self._enable_rgbd or 
-            (self._received_image_color and self._received_image_depth)
+        self._collision_sub = rospy.Subscriber(
+            self._ns(TOPICS.COLLISION),
+            Collision,
+            self._cb_collision,
+            tcp_nodelay=True,
         )
-    
-    def _train_mode_wait_condition(self):
-        return (
-            not self._train_mode or
-            (
-                self._received_collision 
-                and self._received_obs_safe_dist 
-                and self._received_ped_safe_dist
-            )
+        self._ped_safe_dist_sub = rospy.Subscriber(
+            self._ns(TOPICS.PED_SAFE_DIST),
+            Collision,
+            self._cb_ped_safe_dist,
+            tcp_nodelay=True,
+        )
+        self._obs_safe_dist_sub = rospy.Subscriber(
+            self._ns(TOPICS.OBS_SAFE_DIST),
+            Collision,
+            self._cb_obs_safe_dist,
+            tcp_nodelay=True,
         )
 
     def wait(self):
@@ -153,7 +109,9 @@ class UnityCollectorUnit(CollectorUnit):
             return
 
         for _ in range(int(MAX_WAIT / SLEEP)):
-            if self._train_mode_wait_condition and self._rgbd_wait_condition():
+            if (self._received_collision
+                and self._received_ped_safe_dist
+                and self._received_obs_safe_dist):
                 return
 
             sleep(SLEEP)
@@ -163,14 +121,6 @@ class UnityCollectorUnit(CollectorUnit):
                     collision=self._received_collision, 
                     ped_safe_dist=self._received_ped_safe_dist, 
                     obs_safe_dist=self._received_obs_safe_dist
-                )}"""
-            if not self._enable_rgbd else
-            f"""Couldn't retrieve data for: {false_params(
-                    collision=self._received_collision, 
-                    ped_safe_dist=self._received_ped_safe_dist, 
-                    obs_safe_dist=self._received_obs_safe_dist, 
-                    image_color=self._received_image_color,
-                    image_depth=self._received_image_depth
                 )}"""
         )
 
@@ -188,25 +138,15 @@ class UnityCollectorUnit(CollectorUnit):
         """
         obs_dict = super().get_observations(obs_dict)
 
-        if self._train_mode:
-            obs_dict.update(
-                {
-                    OBS_DICT_KEYS.COLLSION: self._collision,
-                    OBS_DICT_KEYS.PED_SAFE_DIST: self._ped_safe_dist,
-                    OBS_DICT_KEYS.OBS_SAFE_DIST: self._obs_safe_dist
-                }
-            )
-        if self._enable_rgbd:
-            obs_dict.update(
-                {
-                    OBS_DICT_KEYS.IMAGE_COLOR: self._image_color,
-                    OBS_DICT_KEYS.IMAGE_DEPTH: self._image_depth
-                }
-            )
+        obs_dict.update(
+            {
+                OBS_DICT_KEYS.COLLSION: self._collision,
+                OBS_DICT_KEYS.PED_SAFE_DIST: self._ped_safe_dist,
+                OBS_DICT_KEYS.OBS_SAFE_DIST: self._obs_safe_dist
+            }
+        )
         
         self._received_collision = False
-        self._received_image_color = False
-        self._received_image_depth = False
         self._received_obs_safe_dist = False
         self._received_ped_safe_dist = False
 
@@ -241,25 +181,3 @@ class UnityCollectorUnit(CollectorUnit):
         """
         self._obs_safe_dist = collsion_msg.collision
         self._received_obs_safe_dist = True
-
-    def _cb_image_color(self, image_msg: Image):
-        """
-        Callback function for receiving rgb data. Converts image msg to cv Mat to np ndarray.
-
-        Args:
-            image_msg (Image): Received rgb image message.
-        """
-        cv_mat = self._image_bridge.imgmsg_to_cv2(image_msg)
-        # let the channel dim be the first dim and remove a-channel
-        self._image_color = (np.asarray(cv_mat, dtype=np.float32)[:,:,0:3]).transpose((2, 0, 1))
-        self._received_image_color = True
-
-    def _cb_image_depth(self, image_msg: Image):
-        """Callback function for receiving depth data. Converts image msg to cv Mat to np ndarray.
-
-        Args:
-            image_msg (Image): Received depth image message.
-        """
-        cv_mat = self._image_bridge.imgmsg_to_cv2(image_msg)
-        self._image_depth = np.asarray(cv_mat, dtype=np.float32)
-        self._received_image_depth = True
