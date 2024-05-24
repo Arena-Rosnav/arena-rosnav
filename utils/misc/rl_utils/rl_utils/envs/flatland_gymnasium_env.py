0c19b319756ae2714595ded7d17b31e3b63f266d
tuananhroman <anh.maxiking@hotmail.de>
2024-05-24 14:53:36 +0200

    remove subgoal mode arg

--- utils/misc/rl_utils/rl_utils/envs/flatland_gymnasium_env.py
+++ utils/misc/rl_utils/rl_utils/envs/flatland_gymnasium_env.py
@@ -169,7 +169,6 @@ class FlatlandEnv(gymnasium.Env):
             safe_dist=self.task.robot_managers[0].safe_distance,
             goal_radius=rosparam_get(float, "goal_radius", 0.3),
             max_steps=self._max_steps_per_episode,
-            subgoal_mode=self._obs_unit_kwargs.get("subgoal_mode", False),
             **self._reward_fnc_kwargs,
         )
 
