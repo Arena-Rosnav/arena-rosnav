ab2e1b4d92277fcff97b2f5231d30479e591787b
tuananhroman <anh.maxiking@hotmail.de>
2024-07-18 15:21:57 +0200

    update params spacial horizon and map gen

--- utils/ros/rosnodes/spacial_horizon/src/spacial_horizon_node.cpp
+++ utils/ros/rosnodes/spacial_horizon/src/spacial_horizon_node.cpp
@@ -18,7 +18,7 @@ void SpacialHorizon::init(ros::NodeHandle &nh)
     nh.param("fsm/goal_tolerance", goal_tolerance, 0.2);
     nh.param("fsm/subgoal_tolerance", subgoal_tolerance, 1.0);
     nh.param("fsm/subgoal_pub_period", subgoal_pub_period, 1.0);
-    nh.param("fsm/update_global_period", update_global_period, 2.0);
+    nh.param("fsm/update_global_period", update_global_period, 5.0);
     nh.param("fsm/planning_horizon", planning_horizon, 5.0);
     
     /* ros communication with public node */
