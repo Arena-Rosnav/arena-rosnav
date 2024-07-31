70015f71edf272c4a28070e075c21f0b5ff86b3a
tuananhroman <anh.maxiking@hotmail.de>
2024-07-31 18:44:15 +0200

    reduce env steps on reset

--- utils/misc/rl_utils/rl_utils/envs/flatland_gymnasium_env.py
+++ utils/misc/rl_utils/rl_utils/envs/flatland_gymnasium_env.py
@@ -340,7 +340,7 @@ class FlatlandEnv(gymnasium.Env):
         """
         # make sure all simulation components are ready before first episode
         if self._episode <= 1:
-            for _ in range(6):
+            for _ in range(3):
                 self.agent_action_pub.publish(Twist())
                 self._call_service_takeSimStep()
 
@@ -351,7 +351,7 @@ class FlatlandEnv(gymnasium.Env):
         """
         if self._is_train_mode:
             # extra step for planning serivce to provide global plan
-            for _ in range(5):
+            for _ in range(2):
                 self.agent_action_pub.publish(Twist())
                 self._call_service_takeSimStep()
 
