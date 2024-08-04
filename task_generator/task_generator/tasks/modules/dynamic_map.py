f01d5e54f12fcf6fe4548ecf5e447720d71e05cb
Tim S <sugerdzn@gmail.com>
2024-08-04 16:08:59 +0200

    dynamic map refactoring for ros2

--- task_generator/task_generator/tasks/modules/dynamic_map.py
+++ task_generator/task_generator/tasks/modules/dynamic_map.py
@@ -1,125 +1,87 @@
 import os
 from typing import Dict
-import map_distance_server.srv as map_distance_server_srvs
-import std_msgs.msg as std_msgs
-import nav_msgs.msg as nav_msgs
-from task_generator.constants import Constants
 
+from rclpy.node import Node
+from rclpy.publisher import Publisher
+from rclpy.subscription import Subscription
+from rclpy.service import Service
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+from std_msgs.msg import String
+from nav_msgs.msg import OccupancyGrid
+from rcl_interfaces.msg import SetParametersResult
+
+from task_generator.constants import Constants
 from task_generator.manager.entity_manager.utils import ObstacleLayer
 from task_generator.manager.utils import WorldMap
-
-from rosros import rospify as rospy
 from task_generator.tasks.modules import TM_Module
 from task_generator.tasks.task_factory import TaskFactory
-from task_generator.utils import rosparam_get
 
-from map_generator.constants import MAP_GENERATOR_NS
+# Assuming map_distance_server messages are available in ROS 2
+from map_distance_server.srv import GetDistanceMap
 
 # DYNAMIC MAP INTERFACE
 
 DynamicMapConfiguration = Dict[str, Dict]
 
+# Define MAP_GENERATOR_NS as a function if it's not available
+def MAP_GENERATOR_NS(param_name: str) -> str:
+    return f"/map_generator/{param_name}"
 
 @TaskFactory.register_module(Constants.TaskMode.TM_Module.DYNAMIC_MAP)
-class Mod_DynamicMap(TM_Module):
-    """
-    This class represents a module for generating and managing dynamic maps in the task generator.
-    It provides functionality for requesting new maps, resetting tasks, and updating the map based on distance information.
-    """
+class Mod_DynamicMap(TM_Module, Node):
+    def __init__(self, **kwargs):
+        TM_Module.__init__(self, **kwargs)
+        Node.__init__(self, 'mod_dynamic_map_node')
 
-    __map_request_pub: rospy.Publisher
-    __task_reset_pub: rospy.Publisher
-    __get_dist_map_service: rospy.ServiceProxy
+        self.callback_group = ReentrantCallbackGroup()
 
-    PARAM_MAP_FILE = "map_file"
-    PARAM_EPISODES = "/dynamic_map/curr_eps"
-    PARAM_GENERATOR = "generator"
-    PARAM_GENERATOR_CONFIGS = "/generator_configs"
+        self.__map_request_pub = self.create_publisher(String, self.TOPIC_REQUEST_MAP, 1)
+        self.__task_reset_pub = self.create_publisher(String, self.TOPIC_RESET, 1)
 
-    TOPIC_REQUEST_MAP = "/request_new_map"
-    TOPIC_RESET = "/dynamic_map/task_reset"
-    TOPIC_MAP = "/map"
-    TOPIC_SIGNAL_MAP = "/signal_new_distance_map"
+        self.create_subscription(String, self.TOPIC_RESET, self._cb_task_reset, 1, callback_group=self.callback_group)
 
-    SERVICE_DISTANCE_MAP = "/distance_map"
+        self.__get_dist_map_service = self.create_client(GetDistanceMap, self.SERVICE_DISTANCE_MAP, callback_group=self.callback_group)
+
+        self.declare_parameter('num_envs', 1)
+        self.declare_parameter(MAP_GENERATOR_NS('episode_per_map'), 1)
+
+        num_envs = self.get_parameter('num_envs').value if "eval_sim" not in self._TASK.robot_managers[0].namespace else 1
+        self._target_eps_num = self.get_parameter(MAP_GENERATOR_NS('episode_per_map')).value * num_envs
+
+        self._episodes = 0
+        self.add_on_set_parameters_callback(self.parameters_callback)
+
+    def parameters_callback(self, params):
+        for param in params:
+            if param.name == self.PARAM_EPISODES:
+                self._episodes = param.value
+        return SetParametersResult(successful=True)
 
     def before_reset(self, **kwargs):
-        """
-        This method is called before resetting the task.
-        It increments the number of episodes and requests a new map if the episode count exceeds the threshold.
-        """
         self._episodes += 1
-
         if self._episodes >= self._target_eps_num:
             self.request_new_map()
 
-    def __init__(self, **kwargs):
-        """
-        Initializes the Mod_DynamicMap module.
-        """
-        TM_Module.__init__(self, **kwargs)
-
-        rospy.Subscriber(self.TOPIC_RESET, std_msgs.String, self._cb_task_reset)
-
-        # requests new map from map generator
-        self.__map_request_pub = rospy.Publisher(
-            self.TOPIC_REQUEST_MAP, std_msgs.String, queue_size=1
-        )
-        # task reset for all taskmanagers when one resets
-        self.__task_reset_pub = rospy.Publisher(
-            self.TOPIC_RESET, std_msgs.String, queue_size=1
-        )
-
-        self.__get_dist_map_service = rospy.ServiceProxy(
-            self.SERVICE_DISTANCE_MAP, map_distance_server_srvs.GetDistanceMap
-        )
-
-        num_envs: int = (
-            rosparam_get(int, "num_envs", 1)
-            if "eval_sim" not in self._TASK.robot_managers[0].namespace
-            else 1
-        )
-        self._target_eps_num = (
-            rosparam_get(int, MAP_GENERATOR_NS("episode_per_map"), 1) * num_envs
-        )
-
     def _set_config(self, config: DynamicMapConfiguration):
-        """
-        Sets the configuration for the map generator based on the provided DynamicMapConfiguration object.
-        """
-        generator = rosparam_get(str, self.PARAM_GENERATOR)
-
+        generator = self.get_parameter(self.PARAM_GENERATOR).value
         log = f"Setting [Map Generator: {generator}] parameters"
-
         config_generator = config.get(generator, dict())
-
         for key, value in config_generator.items():
             log += f"\t{key}={value}"
-            rospy.set_param(
-                os.path.join(self.PARAM_GENERATOR_CONFIGS, generator, key), value
-            )
+            self.set_parameter(f"{self.PARAM_GENERATOR_CONFIGS}.{generator}.{key}", value)
+        self.get_logger().info(log)
 
-        rospy.loginfo(log)
-
-    def _cb_task_reset(self, *args, **kwargs):
-        """
-        Callback function for task reset.
-        Updates the map manager and triggers map update.
-        """
-        # task reset for all taskmanagers when one resets
-        # update map manager
-
+    def _cb_task_reset(self, msg):
         self._update_map()
 
     def _update_map(self):
-        """
-        Updates the map based on the distance information received from the service.
-        """
-        dist_map: map_distance_server_srvs.GetDistanceMapResponse = (
-            self.__get_dist_map_service()
-        )
+        future = self.__get_dist_map_service.call_async(GetDistanceMap.Request())
+        future.add_done_callback(self._update_map_callback)
 
-        if isinstance(dist_map, map_distance_server_srvs.GetDistanceMapResponse):
+    def _update_map_callback(self, future):
+        try:
+            dist_map = future.result()
             self._TASK.world_manager.update_world(
                 world_map=WorldMap.from_occupancy_grid(occupancy_grid=dist_map)
             )
@@ -127,48 +89,45 @@ class Mod_DynamicMap(TM_Module):
             self._TASK.obstacle_manager.spawn_world_obstacles(
                 self._TASK.world_manager.world
             )
+        except Exception as e:
+            self.get_logger().warn(f"Service call failed {e}")
 
     def request_new_map(self):
-        """
-        Requests a new map from the map generator.
-        """
-        # set current eps immediately to 0 so that only one task
-        # requests a new map
         self._episodes = 0
-
-        self.__map_request_pub.publish("")
-
+        self.__map_request_pub.publish(String())
+        
         try:
-            rospy.wait_for_message(self.TOPIC_MAP, nav_msgs.OccupancyGrid, timeout=60)
-            rospy.wait_for_message(self.TOPIC_SIGNAL_MAP, std_msgs.String, timeout=120)
-        except rospy.ROSException:
-            rospy.logwarn(
-                "[Map Generator] Timeout while waiting for new map. Continue with current map."
+            self.create_subscription(
+                OccupancyGrid,
+                self.TOPIC_MAP,
+                lambda _: None,
+                1,
+                callback_group=self.callback_group
             )
+            self.create_subscription(
+                String,
+                self.TOPIC_SIGNAL_MAP,
+                lambda _: None,
+                1,
+                callback_group=self.callback_group
+            )
+        except Exception as e:
+            self.get_logger().warn(f"[Map Generator] Timeout while waiting for new map. Continue with current map. {e}")
         else:
-            rospy.loginfo("===================")
-            rospy.loginfo("+++ Got new map +++")
-            rospy.loginfo("===================")
+            self.get_logger().info("===================")
+            self.get_logger().info("+++ Got new map +++")
+            self.get_logger().info("===================")
 
-        self.__task_reset_pub.publish("")
+        self.__task_reset_pub.publish(String())
 
-    @property
-    def _episodes(self) -> float:
-        """
-        Property representing the current number of episodes.
-        """
-        try:
-            return rosparam_get(float, self.PARAM_EPISODES, float("inf"))
-        except Exception as e:
-            rospy.logwarn(e)
-            return 0
+    PARAM_MAP_FILE = "map_file"
+    PARAM_EPISODES = "dynamic_map.curr_eps"
+    PARAM_GENERATOR = "generator"
+    PARAM_GENERATOR_CONFIGS = "generator_configs"
 
-    @_episodes.setter
-    def _episodes(self, value: float):
-        """
-        Setter for the _episodes property.
-        """
-        try:
-            rospy.set_param(self.PARAM_EPISODES, value)
-        except Exception as e:
-            rospy.logwarn(e)
+    TOPIC_REQUEST_MAP = "/request_new_map"
+    TOPIC_RESET = "/dynamic_map/task_reset"
+    TOPIC_MAP = "/map"
+    TOPIC_SIGNAL_MAP = "/signal_new_distance_map"
+
+    SERVICE_DISTANCE_MAP = "/distance_map"
