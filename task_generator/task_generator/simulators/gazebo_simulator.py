5b0bc3e2df223700682efa1fa02f9442bd73e94b
Tim S <sugerdzn@gmail.com>
2024-08-17 14:12:02 +0200

    adjusted api usage for gazebo

--- task_generator/task_generator/simulators/gazebo_simulator.py
+++ task_generator/task_generator/simulators/gazebo_simulator.py
@@ -1,10 +1,10 @@
 import rclpy
 from rclpy.node import Node
 
-from gazebo_msgs.msg import ModelState
-from gazebo_msgs.srv import SpawnModel, DeleteModel, SetModelState
+from ros_gz_interfaces.srv import SpawnEntity, DeleteEntity, SetEntityPose, ControlWorld
+from ros_gz_interfaces.msg import EntityFactory, WorldControl
 from geometry_msgs.msg import PoseStamped, Pose, Quaternion, Point
-from std_srvs.srv import Empty
+import sys
 
 from task_generator.simulators import SimulatorFactory
 from task_generator.shared import rosparam_get
@@ -33,91 +33,106 @@ class GazeboSimulator(BaseSimulator, Node):
         self.declare_parameter('robot_model', '')
         self._robot_name = self.get_parameter('robot_model').value
 
-        # Initialize service clients
-        self._spawn_model = {
-            ModelType.URDF: self.create_client(SpawnModel, '/gazebo/spawn_urdf_model'),
-            ModelType.SDF: self.create_client(SpawnModel, '/gazebo/spawn_sdf_model'),
-        }
+        # self._spawn_model = {
+        #     ModelType.URDF: self.create_client(SpawnModel, '/gazebo/spawn_urdf_model'),
+        #     ModelType.SDF: self.create_client(SpawnModel, '/gazebo/spawn_sdf_model'),
+        # }
         
-        self._move_model_srv = self.create_client(SetModelState, '/gazebo/set_model_state')
-        self._unpause = self.create_client(Empty, '/gazebo/unpause_physics')
-        self._pause = self.create_client(Empty, '/gazebo/pause_physics')
-        self._remove_model_srv = self.create_client(DeleteModel, '/gazebo/delete_model')
+        # Note: There's no direct equivalent for set_model_state in the new Gazebo
+        # self._move_model_srv = self.create_client(SetModelState, '/gazebo/set_model_state')
+        
+        # Pause and unpause are not directly available in new Gazebo
+        # self._unpause = self.create_client(Empty, '/gazebo/unpause_physics')
+        # self._pause = self.create_client(Empty, '/gazebo/pause_physics')
+        # self._remove_model_srv = self.create_client(DeleteModel, '/gazebo/delete_model')
+        
+        # Initialize service clients
+        # https://gazebosim.org/api/sim/8/entity_creation.html
+        self._spawn_entity = self.create_client(SpawnEntity, '/world/default/create')
+        self._delete_entity = self.create_client(DeleteEntity, '/world/default/remove')
+        self._set_entity_pose = self.create_client(SetEntityPose, '/world/default/set_pose')
+        self._control_world = self.create_client(ControlWorld, '/world/default/control')
         
         self.get_logger().info("Waiting for gazebo services...")
-
         # Wait for services to be available
-        self._unpause.wait_for_service()
-        self._pause.wait_for_service()
-        self._remove_model_srv.wait_for_service()
-        self._spawn_model[ModelType.SDF].wait_for_service()
-        self._move_model_srv.wait_for_service()
+        self._spawn_entity.wait_for_service()
+        self._delete_entity.wait_for_service()
+        self._set_entity_pose.wait_for_service()
+        self._control_world.wait_for_service()
 
         self.get_logger().info("Gazebo services are available now.")
-
+        
 
     def before_reset_task(self):
-        self._pause()
+        self.pause_simulation()
 
     def after_reset_task(self):
         try:
-            self._unpause()
+            self.unpause_simulation()
         except rospy.service.ServiceException as e:  # gazebo isn't the most reliable
             rospy.logwarn(e)
 
     # ROBOT
 
     def move_entity(self, name, position):
-        request = SetModelState.Request()
-        request.model_state = ModelState()
-        request.model_state.model_name = name
-        pose = Pose()
-        pose.position.x = position.x
-        pose.position.y = position.y
-        pose.position.z = 0
-        pose.orientation = Quaternion(
-            *quaternion_from_euler(0.0, 0.0, position.orientation, axes="sxyz")
+        request = SetEntityPose.Request()
+        request.entity = name
+        request.pose = Pose(
+            position=Point(x=position.x, y=position.y, z=0),
+            orientation=Quaternion(*quaternion_from_euler(0.0, 0.0, position.orientation, axes="sxyz"))
         )
-        request.model_state.pose = pose
-        request.model_state.reference_frame = "world"
-
-        future = self._move_model_srv.call_async(request)
+        future = self._set_entity_pose.call_async(request)
         rclpy.spin_until_future_complete(self, future)
         return future.result().success
 
     def spawn_entity(self, entity):
-        request = SpawnModel.Request()
-
-        model = entity.model.get(self.MODEL_TYPES)
-
-        request.model_name = entity.name
-        request.model_xml = model.description
-        request.initial_pose = Pose(
-            position=Point(
-                x=entity.position.x,
-                y=entity.position.y,
-                z=0
-            ),
-            orientation=Quaternion(
-                *quaternion_from_euler(0.0, 0.0, entity.position.orientation, axes="sxyz")
-            )
+        request = SpawnEntity.Request()
+        request.entity_factory = EntityFactory()
+        request.entity_factory.name = entity.name
+        request.entity_factory.type = 'sdf'  # or 'urdf' depending on your model type
+        request.entity_factory.sdf = entity.model.get(self.MODEL_TYPES).description
+        request.entity_factory.pose = Pose(
+            position=Point(x=entity.position.x, y=entity.position.y, z=0),
+            orientation=Quaternion(*quaternion_from_euler(0.0, 0.0, entity.position.orientation, axes="sxyz"))
         )
-        request.robot_namespace = Namespace(entity.name)
-        request.reference_frame = "world"
 
         if isinstance(entity, RobotProps):
-            self.declare_parameter(request.robot_namespace("robot_description"), model.description)
-            self.declare_parameter(request.robot_namespace("tf_prefix"), str(request.robot_namespace))
+            self.declare_parameter(Namespace(entity.name)("robot_description"), entity.model.get(self.MODEL_TYPES).description)
+            self.declare_parameter(Namespace(entity.name)("tf_prefix"), str(Namespace(entity.name)))
 
-        future = self._spawn_model[model.type].call_async(request)
+        future = self._spawn_entity.call_async(request)
         rclpy.spin_until_future_complete(self, future)
         return future.result().success
 
 
     def delete_entity(self, name):
-        request = DeleteModel.Request()
-        request.model_name = name
-        future = self._remove_model_srv.call_async(request)
+        request = DeleteEntity.Request()
+        request.entity = name
+        future = self._delete_entity.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result().success
+    
+    def pause_simulation(self):
+        request = ControlWorld.Request()
+        request.world_control = WorldControl()
+        request.world_control.pause = True
+        future = self._control_world.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result().success
+
+    def unpause_simulation(self):
+        request = ControlWorld.Request()
+        request.world_control = WorldControl()
+        request.world_control.pause = False
+        future = self._control_world.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+        return future.result().success
+
+    def step_simulation(self, steps):
+        request = ControlWorld.Request()
+        request.world_control = WorldControl()
+        request.world_control.multi_step = steps
+        future = self._control_world.call_async(request)
         rclpy.spin_until_future_complete(self, future)
         return future.result().success
 
