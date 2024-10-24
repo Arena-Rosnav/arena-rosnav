ba4c5fefec3f18ffa14996b43450e3c5919f125f
tuananhroman <anh.maxiking@hotmail.de>
2024-10-24 12:23:40 +0200

    model utils

--- training/tools/model_utils.py
+++ training/tools/model_utils.py
@@ -1,67 +1,91 @@
-import os
-from pathlib import Path
-import sys
-from typing import Callable, List, Union
+import re
+from typing import List, TYPE_CHECKING
 
+import rospy
 import torch
 
-from rl_utils.utils.observation_collector.traversal import get_required_observations
-from rosnav.model.agent_factory import AgentFactory
-from rosnav.rosnav_space_manager.rosnav_space_manager import RosnavSpaceManager
-from rosnav.utils.observation_space.observation_space_manager import (
-    ObservationSpaceManager,
+from rl_utils.stable_baselines3.eval_callbacks.staged_train_callback import (
+    InitiateNewTrainStage,
 )
-import rospy
-import wandb
-from rl_utils.utils.eval_callbacks.staged_train_callback import InitiateNewTrainStage
-from rl_utils.utils.learning_rate_schedules import linear_decay, square_root_decay
-from rosnav.model.base_agent import BaseAgent
+from rosnav_rl.utils.model.sb3.learning_rate_schedules import load_lr_schedule
 from sb3_contrib import RecurrentPPO
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import (
     EvalCallback,
     StopTrainingOnRewardThreshold,
 )
-from stable_baselines3.common.policies import ActorCriticPolicy
-from stable_baselines3.common.utils import configure_logger
+from stable_baselines3.common.utils import configure_logger, constant_fn
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv
-from stable_baselines3.common.vec_env.vec_normalize import VecNormalize
-import re
 
-from stable_baselines3.common.utils import constant_fn
+import wandb
 
-from task_generator.shared import Namespace
+if TYPE_CHECKING:
+    from rl_utils.cfg import TrainingCfg, CallbacksCfg
+    from rosnav_rl.model.model import RL_Model
 
 
-def setup_wandb(config: dict, agent: PPO) -> None:
-    wandb.login(key="58b5a2040f5cc9d5c3a7d6102877515716298192")
+def setup_wandb(train_cfg: "TrainingCfg", rl_model: "RL_Model") -> None:
+    """
+    Set up Weights and Biases (wandb) for tracking and visualizing training.
+
+    This function logs into wandb, initializes a new run with the given training
+    configuration, and sets up monitoring for TensorBoard and Gym environments.
+    It also watches the policy model for logging gradients and parameters.
+
+    Args:
+        train_cfg (TrainingCfg): The training configuration object containing
+            settings and parameters for the training session.
+        rl_model (RL_Model): The reinforcement learning model to be tracked
+            and monitored by wandb.
+
+    Returns:
+        None
+    """
+    wandb.login()
     wandb.init(
+        name=train_cfg.agent_cfg.name,
         project="Arena-RL",
         entity=None,
         sync_tensorboard=True,
         monitor_gym=True,
         save_code=True,
-        config=config,
+        config=train_cfg.model_dump(),
     )
-    wandb.watch(agent.policy)
+    wandb.watch(rl_model.model.policy)
 
 
 def check_batch_size(n_envs: int, batch_size: int, mn_batch_size: int) -> None:
-    assert (
-        batch_size >= mn_batch_size
-    ), f"Mini batch size {mn_batch_size} is bigger than batch size {batch_size}"
+    """
+    Validates the batch size against the number of environments and mini batch size.
 
-    assert (
-        batch_size % mn_batch_size == 0
-    ), f"Batch size {batch_size} isn't divisible by mini batch size {mn_batch_size}"
+    Parameters:
+    n_envs (int): Number of environments.
+    batch_size (int): The total batch size.
+    mn_batch_size (int): The mini batch size.
 
-    assert (
-        batch_size % n_envs == 0
-    ), f"Batch size {batch_size} isn't divisible by n_envs {n_envs}"
+    Raises:
+    ValueError: If any of the following conditions are met:
+        - The mini batch size is greater than the batch size.
+        - The batch size is not divisible by the mini batch size.
+        - The batch size is not divisible by the number of environments.
+    """
+    errors = []
 
-    assert (
-        batch_size % mn_batch_size == 0
-    ), f"Batch size {batch_size} isn't divisible by mini batch size {mn_batch_size}"
+    if batch_size < mn_batch_size:
+        errors.append(
+            f"Mini batch size {mn_batch_size} is bigger than batch size {batch_size}"
+        )
+
+    if batch_size % mn_batch_size != 0:
+        errors.append(
+            f"Batch size {batch_size} isn't divisible by mini batch size {mn_batch_size}"
+        )
+
+    if batch_size % n_envs != 0:
+        errors.append(f"Batch size {batch_size} isn't divisible by n_envs {n_envs}")
+
+    if errors:
+        raise ValueError(" | ".join(errors))
 
 
 def update_hyperparam_model(model: PPO, PATHS: dict, config: dict) -> None:
@@ -132,235 +156,52 @@ def update_hyperparam_model(model: PPO, PATHS: dict, config: dict) -> None:
     print("--------------------------------\n")
 
 
-def load_lr_schedule(config: dict) -> Callable:
-    lr_schedule_cfg = config["rl_agent"]["lr_schedule"]
-    if lr_schedule_cfg["type"] == "linear":
-        return linear_decay(**lr_schedule_cfg["settings"])
-    elif lr_schedule_cfg["type"] == "square_root":
-        return square_root_decay(**lr_schedule_cfg["settings"])
-    else:
-        raise NotImplementedError(
-            f"Learning rate schedule '{lr_schedule_cfg['type']}' not implemented!"
-        )
-
-
-def save_model(model: PPO, paths: dict, file_name: str = "best_model") -> None:
-    print("Saving model to: ", paths["model"])
-    model.save(os.path.join(paths["model"], file_name))
-
-    if isinstance(model.env, VecNormalize):
-        model.env.save(os.path.join(paths["model"], f"vec_normalize_{file_name}.pkl"))
-    elif isinstance(model.env.venv, VecNormalize):
-        model.env.venv.save(
-            os.path.join(paths["model"], f"vec_normalize_{file_name}.pkl")
-        )
-
-
-def freeze_weights(model: PPO, include: List[str] = None, exclude: List[str] = None):
-    if include is None:
-        rospy.logwarn("No include list provided. Skipping weight freezing.")
-        return model
-
-    exclude = exclude or []
-
-    if len(exclude) == 0 or exclude[0].lower() == "none" or exclude[0] == "":
-        exclude = ["---"]
-
-    state_dict_model = model.policy.state_dict()
-
-    weights_dict = {
-        key: value
-        for key, value in state_dict_model.items()
-        if any(re.match(_key, key) for _key in include)
-        and not any(item in key for item in exclude)
-    }
-
-    rospy.loginfo(f"Freezing weights for {len(weights_dict.keys())} keys!")
-
-    for name, param in model.policy.features_extractor.named_parameters():
-        if name in weights_dict.keys():
-            param.requires_grad = False
-
-    return model
-
-
-def get_ppo_instance(
-    agent_description: BaseAgent,
-    observation_space_manager,
-    config: dict,
+def init_sb3_callbacks(
     train_env: VecEnv,
-    paths: dict,
-) -> PPO:
-    new_model: bool = (
-        config["rl_agent"]["architecture_name"] and not config["rl_agent"]["resume"]
-    )
-    if new_model:
-        model = instantiate_new_model(
-            agent_description, observation_space_manager, config, train_env, paths
-        )
-    else:
-        model_path = Path(paths["model"]) / f"{config['rl_agent']['checkpoint']}.zip"
-        model = load_model(
-            path=model_path,
-            env=train_env,
-            observation_space_manager=observation_space_manager,
-        )
-        update_hyperparam_model(model, paths, config)
-
-    wandb_logging: bool = not config["debug_mode"] and config["monitoring"]["use_wandb"]
-
-    if wandb_logging:
-        setup_wandb(config, model)
-
-    model2_path = config["rl_agent"]["weight_transfer"]["model_path"]
-
-    if type(model2_path) is str and len(model2_path) > 0 and new_model:
-        transfer_weights(
-            model1=model,
-            model2=load_model(
-                path=model2_path,
-                # env=train_env,
-                observation_space_manager=observation_space_manager,
-            ),
-            include=config["rl_agent"]["weight_transfer"]["include"],
-            exclude=config["rl_agent"]["weight_transfer"]["exclude"],
-        )
-
-    ## Save model once
-
-    # if not rospy.get_param("debug_mode") and new_model:
-    #     save_model(model, paths)
-    # freeze_weights(model=model, include=["^features_extractor"])
-
-    return model
-
-
-def instantiate_new_model(
-    agent_description: BaseAgent,
-    observation_space_manager,
-    config: dict,
-    train_env: VecEnv,
-    PATHS: dict,
-) -> PPO:
-    ppo_config = config["rl_agent"]["ppo"].copy()
-    ppo_config["batch_size"] = ppo_config["m_batch_size"]
-    del ppo_config["m_batch_size"]
-
-    # potentially get learning rate schedule
-    learning_rate = (
-        load_lr_schedule(config)
-        if config["rl_agent"]["lr_schedule"]["enabled"]
-        else ppo_config["learning_rate"]
-    )
-    ppo_config["learning_rate"] = learning_rate
-
-    ppo_kwargs = {
-        "env": train_env,
-        **ppo_config,
-        "tensorboard_log": PATHS["tb"],
-        # "use_wandb": False if config["debug_mode"] else config["monitoring"]["use_wandb"],
-        "verbose": config["monitoring"]["cmd_line_logging"]["training_metrics"][
-            "enabled"
-        ],
-        "device": "cpu" if config["no_gpu"] else "auto",
-    }
-
-    if isinstance(agent_description, BaseAgent):
-        ppo_kwargs["policy"] = agent_description.type.value
-
-        frame_stacking_cfg = config["rl_agent"]["frame_stacking"]
-        # get policy description kwargs
-        policy_kwargs = agent_description.get_kwargs(
-            observation_space_manager=observation_space_manager,
-            stack_size=(
-                frame_stacking_cfg["stack_size"] if frame_stacking_cfg["enabled"] else 1
-            ),
-        )
-        ppo_kwargs["policy_kwargs"] = policy_kwargs
-    elif issubclass(agent_description, ActorCriticPolicy):
-        raise NotImplementedError("ActorCriticPolicy not implemented yet!")
-        ppo_kwargs["policy"] = agent_description
-    else:
-        arch_name = config["rl_agent"]["architecture_name"]
-        raise TypeError(
-            f"Registered agent class {arch_name} is neither of type"
-            "'BaseAgent' or 'ActorCriticPolicy'!"
-        )
-
-    is_lstm = "LSTM" in agent_description.type.name
-    return RecurrentPPO(**ppo_kwargs) if is_lstm else PPO(**ppo_kwargs)
-
-
-def load_model(
-    path: str,
-    env: VecEnv = None,
-    observation_space_manager: ObservationSpaceManager = None,
-) -> Union[RecurrentPPO, PPO]:
-    if not os.path.isfile(path):
-        raise FileNotFoundError(f"Model file {path} not found!")
-
-    path = str(path)
-    config_path = path[: path.rfind("/") + 1] + "training_config.yaml"
-    # read yaml config
-    with open(config_path, "r") as file:
-        config = yaml.safe_load(file)
-
-    agent_description = AgentFactory.instantiate(
-        config["rl_agent"]["architecture_name"]
-    )
-
-    frame_stacking_cfg = config["rl_agent"]["frame_stacking"]
-
-    # Instantiate ObservationSpaceManager
-    if observation_space_manager is None:
-        raise ValueError("ObservationSpaceManager must be provided!")
-
-    # DYNAMIC POLICY UPDATE WHEN LOADING MODEL
-    is_lstm = "LSTM" in agent_description.type.name
-
-    custom_objects = {
-        "policy_kwargs": agent_description.get_kwargs(
-            observation_space_manager=observation_space_manager,
-            stack_size=(
-                frame_stacking_cfg["stack_size"]
-                if frame_stacking_cfg["enabled"] and not is_lstm
-                else 1
-            ),
-        ),
-        "observation_space": observation_space_manager.observation_space,
-    }
-
-    # load model
-    return (
-        RecurrentPPO.load(path, env=env, custom_objects=custom_objects)
-        if is_lstm
-        else PPO.load(path, env=env, custom_objects=custom_objects)
-    )
-
-
-def init_callbacks(
-    config: dict, train_env: VecEnv, eval_env: VecEnv, paths
+    eval_env: VecEnv,
+    n_envs: int,
+    tm_modules: List[str],
+    model_save_path: str,
+    eval_log_path: str,
+    callback_cfg: "CallbacksCfg",
+    debug_mode: bool,
 ) -> EvalCallback:
+    """
+    Initialize Stable Baselines3 (SB3) callbacks for training and evaluation.
+
+    Args:
+        train_env (VecEnv): The training environment.
+        eval_env (VecEnv): The evaluation environment.
+        n_envs (int): Number of environments.
+        tm_modules (List[str]): List of training modules.
+        model_save_path (str): Path to save the model.
+        eval_log_path (str): Path to save evaluation logs.
+        callback_cfg (CallbacksCfg): Configuration for callbacks.
+        debug_mode (bool): If True, disables model saving for debugging purposes.
+
+    Returns:
+        EvalCallback: The evaluation callback configured with the specified settings.
+    """
     # threshold settings for training curriculum
     # type can be either 'succ' or 'rew'
-    curriculum_cfg = config["callbacks"]["training_curriculum"]
-    stop_train_cfg = config["callbacks"]["stop_training"]
-    periodic_eval_cfg = config["callbacks"]["periodic_eval"]
+    curriculum_cfg = callback_cfg.training_curriculum
+    stop_train_cfg = callback_cfg.stop_training_on_threshold
+    periodic_eval_cfg = callback_cfg.periodic_evaluation
 
     trainstage_cb = InitiateNewTrainStage(
-        n_envs=config["n_envs"],
-        treshhold_type=curriculum_cfg["threshold_type"],
-        upper_threshold=curriculum_cfg["upper_threshold"],
-        lower_threshold=curriculum_cfg["lower_threshold"],
-        activated="staged" in config["tm_modules"],
+        n_envs=n_envs,
+        treshhold_type=curriculum_cfg.threshold_type,
+        upper_threshold=curriculum_cfg.upper_threshold,
+        lower_threshold=curriculum_cfg.lower_threshold,
+        activated="staged" in tm_modules,
         verbose=1,
     )
 
     # stop training on reward threshold callback
     stoptraining_cb = StopTrainingOnRewardThreshold(
-        treshhold_type=stop_train_cfg["threshold_type"],
-        threshold=stop_train_cfg["threshold"],
-        verbose=1,
+        treshhold_type=stop_train_cfg.threshold_type,
+        threshold=stop_train_cfg.threshold,
+        verbose=stop_train_cfg.verbose,
     )
 
     # evaluation settings
@@ -369,15 +210,14 @@ def init_callbacks(
     eval_cb = EvalCallback(
         eval_env=eval_env,
         train_env=train_env,
-        n_eval_episodes=periodic_eval_cfg["n_eval_episodes"],
-        eval_freq=periodic_eval_cfg["eval_freq"],
-        log_path=paths["eval"],
-        best_model_save_path=None if config["debug_mode"] else paths["model"],
+        n_eval_episodes=periodic_eval_cfg.n_eval_episodes,
+        eval_freq=periodic_eval_cfg.eval_freq,
+        log_path=eval_log_path,
+        best_model_save_path=None if debug_mode else model_save_path,
         deterministic=True,
         callback_on_eval_end=trainstage_cb,
         callback_on_new_best=stoptraining_cb,
     )
-
     return eval_cb
 
 
@@ -440,16 +280,40 @@ def freeze_weights(model: PPO, include: List[str] = None, exclude: List[str] = N
     return model
 
 
-from sb3_contrib.common.recurrent.type_aliases import RNNStates
+import yaml
+from gymnasium import spaces
 from sb3_contrib.common.recurrent.buffers import (
     RecurrentDictRolloutBuffer,
     RecurrentRolloutBuffer,
 )
-from gymnasium import spaces
-import yaml
+from sb3_contrib.common.recurrent.type_aliases import RNNStates
 
 
 def init_rppo_rollout_buffer(model: RecurrentPPO):
+    """
+    Initialize the rollout buffer for a Recurrent PPO (Proximal Policy Optimization) model.
+
+    This function sets up the appropriate rollout buffer class based on the type of observation space
+    (either a dictionary or a standard space). It also initializes the hidden and cell states for the
+    LSTM (Long Short-Term Memory) layers used in the actor and critic networks of the model.
+
+    Args:
+        model (RecurrentPPO): The Recurrent PPO model for which the rollout buffer is being initialized.
+
+    Attributes:
+        model._last_lstm_states (RNNStates): A tuple containing the hidden and cell states for both the
+                                             actor and critic LSTM networks, initialized to zeros.
+        model.rollout_buffer (RecurrentRolloutBuffer or RecurrentDictRolloutBuffer): The rollout buffer
+                                                                                     instance used for
+                                                                                     storing experiences
+                                                                                     during training.
+
+    Notes:
+        - The shape of the hidden and cell states is determined by the number of LSTM layers, the number
+          of environments, and the hidden size of the LSTM.
+        - The rollout buffer is initialized with parameters such as the number of steps, observation space,
+          action space, hidden state buffer shape, device, gamma, GAE lambda, and the number of environments.
+    """
     buffer_cls = (
         RecurrentDictRolloutBuffer
         if isinstance(model.observation_space, spaces.Dict)
@@ -487,9 +351,3 @@ def init_rppo_rollout_buffer(model: RecurrentPPO):
         gae_lambda=model.gae_lambda,
         n_envs=model.n_envs,
     )
-
-
-def load_policies():
-    import rosnav.model.custom_policy
-    import rosnav.model.custom_sb3_policy
-    import rosnav.model.sb3_policy.paper
