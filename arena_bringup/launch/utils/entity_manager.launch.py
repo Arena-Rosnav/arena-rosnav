7c171211eaa162ee30cef3e9aece07e77be1687c
AhmedMartban <ahmed_martban@hotmail.de>
2024-10-26 03:57:25 +0200

    add hunavsim into entity manager

--- arena_bringup/launch/utils/entity_manager.launch.py
+++ arena_bringup/launch/utils/entity_manager.launch.py
@@ -3,7 +3,7 @@ import sys
 
 import launch
 import launch_ros.actions
-
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     ld = launch.LaunchDescription([
@@ -27,10 +27,29 @@ def generate_launch_description():
         launch.actions.DeclareLaunchArgument(
             name='pedsim',
             default_value="$(eval entity_manager in ('pedsim', 'crowdsim'))"
+        ),
+        # Add Hunavsim argument
+        launch.actions.DeclareLaunchArgument(
+            name='hunavsim',
+            default_value="$(eval entity_manager == 'hunavsim')"
+        ),
+        
+        # Include Hunavsim launch file if hunavsim is selected
+        launch.actions.IncludeLaunchDescription(
+            launch.launch_description_sources.PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('arena_bringup'),
+                    'launch', 'utils', 'hunavsim.launch.py')
+            ),
+            condition=launch.conditions.IfCondition(
+                launch.substitutions.LaunchConfiguration('hunavsim')
+            ),
+            launch_arguments={
+                'use_sim_time': 'true',
+                'world_file': launch.substitutions.LaunchConfiguration('world_file')
+            }.items()
         )
     ])
     return ld
-
 
 if __name__ == '__main__':
     generate_launch_description()
