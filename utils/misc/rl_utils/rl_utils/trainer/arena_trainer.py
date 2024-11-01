07331eb0b065c122fc2590302ee428b85b4e2ef5
tuananhroman <anh.maxiking@hotmail.de>
2024-11-01 17:57:17 +0100

    functional sb3 trainer with base trainer

--- utils/misc/rl_utils/rl_utils/trainer/arena_trainer.py
+++ utils/misc/rl_utils/rl_utils/trainer/arena_trainer.py
@@ -1,69 +1,198 @@
 import sys
 import time
 from abc import ABC, abstractmethod
-from typing import TypedDict
+from enum import Enum
+from typing import Callable, Dict, List, TypedDict, Union
 
+import gymnasium
+from rl_utils.utils.paths import PathFactory
 import rospy
+from pydantic import BaseModel
+from rl_utils.cfg.train import TrainingCfg
 from rl_utils.state_container import SimulationStateContainer
 from rl_utils.utils.type_alias.observation import PathsDict
 from rosnav_rl.rl_agent import RL_Agent
-from tools.general import save_model_and_exit
+from tools.general import (
+    create_directories,
+    print_base_model,
+    save_model,
+    write_config_yaml,
+)
+
+import rl_utils.utils.paths as Paths
+
+
+class TrainingFramework(Enum):
+    SB3 = "StableBaselines3"
+
+
+class TrainerHook(Enum):
+    ON_INIT = "on_init"
+    BEFORE_SETUP = "before_setup"
+    AFTER_SETUP = "after_setup"
+    BEFORE_TRAINING = "before_training"
+    AFTER_TRAINING = "after_training"
+    ON_SAVE = "on_save"
+    ON_CLOSE = "on_close"
 
 
 class ArenaTrainer(ABC):
-    agent: RL_Agent
+    FRAMEWORK: TrainingFramework
+
+    config: TrainingCfg
     paths: PathsDict
-    method_arguments: TypedDict
 
-    @abstractmethod
-    def _initialize_agent(self, *args, **kwargs):
-        pass
+    simulation_state_container: SimulationStateContainer
+    agent: RL_Agent
+    environment: gymnasium.Env
 
-    def _setup_directories(self, *args, **kwargs):
-        pass
+    __hooks: Dict[TrainerHook, List[Callable]]
 
-    @abstractmethod
-    def _setup_environment(self, *args, **kwargs):
-        pass
+    def __init__(self, config: TrainingCfg):
+        self.config = config
+        self._initialize_hook_stages()
+        self._register_default_hooks()
+        self._run_hooks(TrainerHook.ON_INIT)
 
-    def _setup_monitoring(self, *args, **kwargs):
-        pass
+    def register_hook(self, hook_name: Union[TrainerHook, str], callback: Callable):
+        """Register a new hook callback."""
+        if hook_name not in self.__hooks:
+            print(f"Hook {hook_name} not found. Creating new hook.")
+            self.__hooks[hook_name] = []
+        self.__hooks[hook_name].append(callback)
 
-    @abstractmethod
-    def _init_simulation_state_container(
-        self, *args, **kwargs
-    ) -> SimulationStateContainer:
-        pass
+    def setup(self, *args, **kwargs):
+        """Template method for setting up the trainer."""
+        self._run_hooks(TrainerHook.BEFORE_SETUP)
 
-    @abstractmethod
-    def _prepare_method_arguments(self, *args, **kwargs) -> TypedDict:
-        return TypedDict()
+        self._register_framework_specific_hooks()
+        self._setup_monitoring()
+        self._setup_simulation_state_container()
+        self._setup_agent()
+        self._setup_paths()
+        self._setup_environment()
+        self._setup_train_method_arguments()
 
-    def _train_impl(self, method_args: TypedDict, *args, **kwargs):
-        rospy.on_shutdown(
-            lambda: save_model_and_exit(
-                rl_model=self.agent,
-                dirpath=self.paths["model"],
-                checkpoint_name="last_model",
-            )
-        )
+        self._run_hooks(TrainerHook.AFTER_SETUP)
 
+    def train(self, method_args: TypedDict = None) -> None:
+        """Execute the training process."""
+        self._run_hooks(TrainerHook.BEFORE_TRAINING)
         start = time.time()
-        self.agent.model.train(**method_args)
-        print(
-            f"Time passed: {time.time()-start}s. \n Training script will be terminated.."
-        )
-        self.close()
-
-    def train(
-        self,
-        method_args: TypedDict = None,
-        *args,
-        **kwargs,
-    ) -> callable:
-        self._train_impl(
-            method_args=method_args or self.method_arguments, *args, **kwargs
-        )
+        try:
+            self._train_impl(method_args or getattr(self, "method_arguments", {}))
+        finally:
+            duration = time.time() - start
+            print(f"Time passed: {duration}s. \nTraining script will be terminated..")
+            self._run_hooks(TrainerHook.AFTER_TRAINING)
 
     def close(self):
-        sys.exit(0)
+        """Clean up and exit."""
+        self._run_hooks(TrainerHook.ON_CLOSE)
+
+    def _initialize_hook_stages(self):
+        self.__hooks: Dict[TrainerHook, List[Callable]] = {
+            stage: [] for stage in TrainerHook
+        }
+
+    def _register_default_hooks(self):
+        """Register default hooks that are common across all implementations."""
+        # ON_INIT
+        # self.register_hook(
+        #     TrainerHook.ON_INIT,
+        #     lambda _: print(
+        #         f"________ STARTING TRAINING WITH: {self.agent.name} ________\n"
+        #     ),
+        # )
+        self.register_hook(TrainerHook.ON_INIT, lambda _: print_base_model(self.config))
+
+        # BEFORE_SETUP
+        self.register_hook(
+            TrainerHook.BEFORE_SETUP, lambda _: self._setup_node(self.is_debug_mode)
+        )
+
+        # AFTER_SETUP
+        self.register_hook(
+            TrainerHook.AFTER_SETUP,
+            lambda _: self._write_config(debug_mode=self.is_debug_mode),
+        )
+
+        # AFTER_TRAINING
+        self.register_hook(
+            TrainerHook.AFTER_TRAINING,
+            lambda: rospy.on_shutdown(lambda: self._run_hooks(TrainerHook.ON_SAVE)),
+        )
+
+        # ON_SAVE
+        self.register_hook(
+            TrainerHook.ON_SAVE,
+            lambda: save_model(
+                rl_model=self.agent,
+                dirpath=self.paths[Paths.Agent],
+                checkpoint_name="last_model",
+                is_debug_mode=self.is_debug_mode,
+            ),
+        )
+
+        # ON_CLOSE
+        self.register_hook(TrainerHook.ON_CLOSE, lambda _: self.agent.model.env.close())
+        self.register_hook(TrainerHook.ON_CLOSE, lambda _: sys.exit(0))
+
+    def _run_hooks(self, hook_name: Union[TrainerHook, str]):
+        """Execute all callbacks registered for a specific hook."""
+        for callback in self.__hooks.get(hook_name, [lambda: None]):
+            callback(self)
+
+    def _setup_node(self, debug_mode: bool = False):
+        """Setup ROS node."""
+        rospy.set_param("debug_mode", debug_mode)
+        if debug_mode:
+            rospy.init_node("debug_node", disable_signals=False)
+
+    def _write_config(self, debug_mode: bool = False):
+        """Write configuration to file if not in debug mode."""
+        if not debug_mode:
+            write_config_yaml(
+                self.config.model_dump(),
+                self.paths[Paths.Agent] / "training_config.yaml",
+            )
+
+    def _setup_paths(self, *args, **kwargs):
+        """Setup necessary directories."""
+        self.paths: PathsDict = PathFactory.get_paths(self.agent.name)
+
+    def _register_framework_specific_hooks(self):
+        """Register hooks that are specific to the framework."""
+        pass
+
+    @abstractmethod
+    def _setup_agent(self, *args, **kwargs) -> None:
+        """Initialize the RL agent."""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _setup_environment(self, *args, **kwargs) -> None:
+        """Setup training Gym environment."""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _setup_monitoring(self, *args, **kwargs):
+        """Setup monitoring tools."""
+        raise NotImplementedError()
+
+    @abstractmethod
+    def _setup_simulation_state_container(self, *args, **kwargs):
+        """Initialize simulation state container."""
+        raise NotImplementedError()
+
+    def _setup_train_method_arguments(self, *args, **kwargs):
+        """Prepare arguments for training method."""
+        self.method_arguments = {}
+
+    def _train_impl(self, method_args: TypedDict):
+        """Implementation of training logic."""
+        self.agent.model.train(**method_args)
+
+    @property
+    def is_debug_mode(self):
+        return rospy.get_param("debug_mode", False)
--- utils/misc/rl_utils/rl_utils/trainer/sb3_trainer.py
+++ utils/misc/rl_utils/rl_utils/trainer/sb3_trainer.py
@@ -1,212 +1,125 @@
+import os
 from typing import Literal, Tuple, TypedDict
 
 import rl_utils.cfg as cfg
 import rl_utils.cfg.sb3_cfg.algorithms.ppo.args as ppo_args
+import rl_utils.utils.paths as Paths
 import rosnav_rl.cfg as rl_cfg
 import rosnav_rl.rl_agent as Rosnav_RL
 import rospy
-from rl_utils.cfg import SB3TrainingCfg
+from rl_utils.cfg import TrainingCfg
 from rl_utils.state_container import SimulationStateContainer
-from rl_utils.trainer.arena_trainer import ArenaTrainer
-from rl_utils.utils.type_alias.observation import PathsDict
-from rosnav_rl.rl_agent import RL_Agent
-from stable_baselines3.common.callbacks import EvalCallback
-from stable_baselines3.common.vec_env import VecEnv
-from tools.config import load_training_config, unpack_framework_config
+from rl_utils.trainer.arena_trainer import ArenaTrainer, TrainerHook, TrainingFramework
+from tools.config import load_training_config, unpack_sb3_config, ConfigUnpacker
 from tools.env_utils import make_envs
-from tools.general import (
-    create_directories,
-    get_paths,
-    print_base_model,
-    write_config_yaml,
-)
 from tools.model_utils import init_sb3_callbacks, setup_wandb
 from tools.states import get_arena_states
 
 
 class StableBaselines3Trainer(ArenaTrainer):
-    FRAMEWORK: Literal["SB3"] = "SB3"
-    agent: RL_Agent
-    paths: PathsDict
-    simulation_state_container: SimulationStateContainer
-    config: SB3TrainingCfg
+    FRAMEWORK: TrainingFramework = TrainingFramework.SB3
 
-    def __init__(self, config: SB3TrainingCfg) -> None:
-        self.config = config
+    def __init__(self, config: TrainingCfg):
+        super().__init__(config)
+        self._unpack_config()
+        self.setup()
 
-        print(f"________ STARTING TRAINING WITH:  {config.agent_cfg.name} ________\n")
-        print_base_model(config)
-
+    def _unpack_config(self):
+        """Unpack configuration into instance variables."""
         (
-            general_cfg,
-            callbacks_cfg,
-            monitoring_cfg,
-            task_cfg,
-            robot_cfg,
-            normalization_cfg,
-            profiling_cfg,
-        ) = unpack_framework_config(config.framework_cfg)
-        agent_cfg: rl_cfg.AgentCfg = config.agent_cfg
+            self.general_cfg,
+            self.monitoring_cfg,
+            self.task_cfg,
+            self.normalization_cfg,
+            self.callbacks_cfg,
+            self.profiling_cfg,
+            self.robot_cfg,
+        ) = unpack_sb3_config(self.config.framework_cfg)
+        self.agent_cfg: rl_cfg.AgentCfg = self.config.agent_cfg
 
-        self.__setup_node(general_cfg.debug_mode)
-        self.paths = self._setup_directories(
-            agent_cfg=agent_cfg,
-            callbacks_cfg=callbacks_cfg,
-            monitoring_cfg=monitoring_cfg,
-            general_cfg=general_cfg,
+    def _register_framework_specific_hooks(self):
+        """Register hooks specific to SB3 implementation."""
+        # BEFORE_TRAINING
+        self.register_hook(
+            TrainerHook.BEFORE_TRAINING,
+            lambda _: os.system(
+                f"rosrun dynamic_reconfigure dynparam set /task_generator_server STAGED_curriculum {self.callbacks_cfg.training_curriculum.curriculum_file}"
+            ),
         )
-
-        if not general_cfg.debug_mode:
-            write_config_yaml(config.model_dump(), self.paths["config"])
-
-        self.simulation_state_container = self._init_simulation_state_container(
-            agent_cfg=agent_cfg,
-            general_cfg=general_cfg,
-            robot_cfg=robot_cfg,
-            task_cfg=task_cfg,
-        )
-        self.agent = self._initialize_agent(
-            agent_cfg=agent_cfg, sim_state_container=self.simulation_state_container
-        )
-
-        train_env, eval_env = self._setup_environment(
-            agent_instance=self.agent,
-            sim_state_container=self.simulation_state_container,
-            agent_cfg=agent_cfg,
-            general_cfg=general_cfg,
-            callbacks_cfg=callbacks_cfg,
-            normalization_cfg=normalization_cfg,
-            monitoring_cfg=monitoring_cfg,
-            profiling_cfg=profiling_cfg,
-        )
-        eval_cb = self._setup_callbacks(
-            train_env=train_env,
-            eval_env=eval_env,
-            general_cfg=general_cfg,
-            task_cfg=task_cfg,
-            callbacks_cfg=callbacks_cfg,
-        )
-
-        # initialize model
-        self.agent.initialize_model(
-            env=train_env,
-            no_gpu=general_cfg.no_gpu,
-            tensorboard_log_path=(
-                self.paths["tb"] if not general_cfg.debug_mode else None
+        self.register_hook(
+            TrainerHook.BEFORE_TRAINING,
+            lambda _: os.system(
+                f"rosrun dynamic_reconfigure dynparam set /task_generator_server STAGED_index {self.callbacks_cfg.training_curriculum.current_stage}"
             ),
         )
 
-        self._setup_monitoring(config=config, agent_instance=self.agent)
-
-        self.method_arguments = self._prepare_method_arguments(
-            eval_cb=eval_cb, general_cfg=general_cfg
+    def _setup_agent(self) -> None:
+        """Setup the RL agent."""
+        self.agent = Rosnav_RL.RL_Agent(
+            agent_cfg=self.agent_cfg,
+            simulation_state_container=self.simulation_state_container,
         )
 
-    def __setup_node(self, debug_mode: bool):
-        rospy.set_param("debug_mode", debug_mode)
-        if debug_mode:
-            rospy.init_node("debug_node", disable_signals=False)
-
-    def _initialize_agent(
-        self, agent_cfg: rl_cfg.AgentCfg, sim_state_container: SimulationStateContainer
-    ) -> Rosnav_RL.RL_Agent:
-        return Rosnav_RL.RL_Agent(
-            agent_cfg=agent_cfg,
-            simulation_state_container=sim_state_container,
+    def _setup_environment(self) -> None:
+        """Setup training environment."""
+        train_env, eval_env = make_envs(
+            rl_agent=self.agent,
+            simulation_state_container=self.simulation_state_container,
+            agent_cfg=self.agent_cfg,
+            general_cfg=self.general_cfg,
+            callback_cfg=self.callbacks_cfg,
+            normalization_cfg=self.normalization_cfg,
+            monitoring_cfg=self.monitoring_cfg,
+            profiling_cfg=self.profiling_cfg,
         )
 
-    def _setup_directories(
-        self,
-        agent_cfg: rl_cfg.AgentCfg,
-        callbacks_cfg: cfg.CallbacksCfg,
-        monitoring_cfg: cfg.MonitoringCfg,
-        general_cfg: cfg.GeneralCfg,
-    ):
-        paths = get_paths(
-            agent_name=agent_cfg.name,
-            curriculum_file=callbacks_cfg.training_curriculum.curriculum_file,
-        )
-        if not general_cfg.debug_mode:
-            create_directories(
-                paths=paths,
-                resume_name=agent_cfg.framework.model.resume,
-                log_evaluation=monitoring_cfg.eval_metrics,
-                use_wandb=monitoring_cfg.wandb,
-            )
-        return paths
-
-    def _setup_environment(
-        self,
-        agent_instance: RL_Agent,
-        sim_state_container: SimulationStateContainer,
-        agent_cfg: rl_cfg.AgentCfg,
-        general_cfg: cfg.GeneralCfg,
-        callbacks_cfg: cfg.CallbacksCfg,
-        normalization_cfg: cfg.NormalizationCfg,
-        monitoring_cfg: cfg.MonitoringCfg,
-        profiling_cfg: cfg.ProfilingCfg,
-    ) -> Tuple[VecEnv, VecEnv]:
-        return make_envs(
-            rl_agent=agent_instance,
-            simulation_state_container=sim_state_container,
-            agent_cfg=agent_cfg,
-            general_cfg=general_cfg,
-            callback_cfg=callbacks_cfg,
-            normalization_cfg=normalization_cfg,
-            monitoring_cfg=monitoring_cfg,
-            profiling_cfg=profiling_cfg,
-        )
-
-    def _setup_callbacks(
-        self,
-        train_env: VecEnv,
-        eval_env: VecEnv,
-        general_cfg: cfg.GeneralCfg,
-        task_cfg: cfg.TaskCfg,
-        callbacks_cfg: cfg.CallbacksCfg,
-    ) -> EvalCallback:
-        return init_sb3_callbacks(
+        self.eval_cb = init_sb3_callbacks(
             train_env=train_env,
             eval_env=eval_env,
-            n_envs=general_cfg.n_envs,
-            tm_modules=task_cfg.tm_modules,
-            callback_cfg=callbacks_cfg,
-            model_save_path=self.paths["model"],
-            eval_log_path=self.paths["eval"],
-            debug_mode=general_cfg.debug_mode,
+            n_envs=self.general_cfg.n_envs,
+            tm_modules=self.task_cfg.tm_modules,
+            callback_cfg=self.callbacks_cfg,
+            model_save_path=self.paths[Paths.Agent],
+            eval_log_path=self.paths[Paths.AgentEval],
+            debug_mode=self.general_cfg.debug_mode,
         )
 
-    def _setup_monitoring(self, config: SB3TrainingCfg, agent_instance: RL_Agent):
-        if (
-            not config.framework_cfg.general.debug_mode
-            and config.framework_cfg.monitoring.wandb
-        ):
-            setup_wandb(train_cfg=config, rl_model=agent_instance.model)
-
-    def _init_simulation_state_container(
-        self,
-        agent_cfg: rl_cfg.AgentCfg,
-        general_cfg: cfg.GeneralCfg,
-        robot_cfg: cfg.RobotCfg,
-        task_cfg: cfg.TaskCfg,
-    ) -> SimulationStateContainer:
-        return get_arena_states(
-            goal_radius=general_cfg.goal_radius,
-            max_steps=general_cfg.max_num_moves_per_eps,
-            is_discrete=agent_cfg.action_space.is_discrete,
-            safety_distance=general_cfg.safety_distance,
-            robot_cfg=robot_cfg,
-            task_modules_cfg=task_cfg,
+        self.agent.initialize_model(
+            env=train_env,
+            no_gpu=self.general_cfg.no_gpu,
+            tensorboard_log_path=(
+                self.paths[Paths.AgentTensorboard]
+                if not self.general_cfg.debug_mode
+                else None
+            ),
         )
 
-    def _prepare_method_arguments(
-        self, eval_cb: EvalCallback, general_cfg: cfg.GeneralCfg
-    ) -> TypedDict:
-        return ppo_args.TrainArguments(
-            total_timesteps=general_cfg.n_timesteps,
-            callback=eval_cb,
-            progress_bar=general_cfg.show_progress_bar,
+    def _setup_simulation_state_container(self) -> SimulationStateContainer:
+        self.simulation_state_container = get_arena_states(
+            goal_radius=self.general_cfg.goal_radius,
+            max_steps=self.general_cfg.max_num_moves_per_eps,
+            is_discrete=self.agent_cfg.action_space.is_discrete,
+            safety_distance=self.general_cfg.safety_distance,
+            robot_cfg=self.robot_cfg,
+            task_modules_cfg=self.task_cfg,
         )
+
+    def _setup_monitoring(self) -> None:
+        """Setup monitoring tools."""
+        if not self.general_cfg.debug_mode and self.monitoring_cfg.wandb:
+            setup_wandb(train_cfg=self.config, rl_model=self.agent.model)
+
+    def _setup_train_method_arguments(self) -> TypedDict:
+        """Prepare arguments for training method."""
+        self.method_arguments = ppo_args.TrainArguments(
+            total_timesteps=self.general_cfg.n_timesteps,
+            callback=self.eval_cb,
+            progress_bar=self.general_cfg.show_progress_bar,
+        )
+
+    @property
+    def is_debug_mode(self) -> bool:
+        return self.general_cfg.debug_mode
 
 
 def main():
