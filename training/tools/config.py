1610833372782649fa5ad100224cfe0a23519a0b
tuananhroman <anh.maxiking@hotmail.de>
2024-11-01 17:55:24 +0100

    config utilies

--- training/tools/config.py
+++ training/tools/config.py
@@ -1,36 +1,67 @@
-from typing import Tuple
+from dataclasses import dataclass
+from typing import Any, Dict, Generic, Tuple, Type, TypeVar
 
 import rl_utils.cfg as cfg
+from pydantic import BaseModel
 
 from tools.constants import TRAINING_CONSTANTS
 
 from .general import load_config
 
+T = TypeVar("T", bound=BaseModel)
 
-def load_training_config(config_name: str) -> cfg.SB3TrainingCfg:
+
+@dataclass
+class ConfigUnpacker(Generic[T]):
+    """
+    A utility class for unpacking Pydantic model configurations.
+
+    This class provides static methods to unpack Pydantic model instances into
+    tuples or dictionaries, as well as to unpack specific fields from the model.
+
+    Methods
+    -------
+    unpack(config: T) -> Tuple[Any, ...]
+        Unpack the entire configuration into a tuple.
+
+    unpack_dict(config: T) -> Dict[str, Any]
+        Unpack the entire configuration into a dictionary.
+
+    unpack_selected(config: T, *fields: str) -> Tuple[Any, ...]
+        Unpack specific fields from the configuration into a tuple.
+    """
+
+    @staticmethod
+    def unpack(config: T) -> Tuple[Any, ...]:
+        """Unpack config into tuple"""
+        return tuple(getattr(config, field) for field in config.__fields__)
+
+    @staticmethod
+    def unpack_dict(config: T) -> Dict[str, Any]:
+        """Unpack config into dictionary"""
+        return {field: getattr(config, field) for field in config.__fields__}
+
+    @staticmethod
+    def unpack_selected(config: T, *fields: str) -> Tuple[Any, ...]:
+        """Unpack specific fields from config"""
+        return tuple(getattr(config, field) for field in fields)
+
+
+def load_training_config(config_name: str) -> cfg.TrainingCfg:
     raw_config = load_config(TRAINING_CONSTANTS.PATHS.TRAINING_CONFIGS(config_name))
-    return cfg.SB3TrainingCfg.model_validate(
-        raw_config, strict=True, from_attributes=True
-    )
+    return cfg.TrainingCfg.model_validate(raw_config, strict=True, from_attributes=True)
 
 
-def unpack_framework_config(
+def unpack_sb3_config(
     framework_cfg: cfg.SB3Cfg,
 ) -> Tuple[
     cfg.GeneralCfg,
-    cfg.CallbacksCfg,
     cfg.MonitoringCfg,
     cfg.TaskCfg,
-    cfg.RobotCfg,
     cfg.NormalizationCfg,
+    cfg.CallbacksCfg,
     cfg.ProfilingCfg,
+    cfg.RobotCfg,
 ]:
-    return (
-        framework_cfg.general,
-        framework_cfg.callbacks,
-        framework_cfg.monitoring,
-        framework_cfg.task,
-        framework_cfg.robot,
-        framework_cfg.normalization,
-        framework_cfg.profiling,
-    )
+    unpacker = ConfigUnpacker[cfg.SB3Cfg]()
+    return unpacker.unpack(framework_cfg)
