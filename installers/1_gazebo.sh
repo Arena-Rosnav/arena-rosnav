d24d9d56006ddf6ca547aa3cd19a036ae832232b
Tim Seeger <t.seeger@tu-berlin.de>
2024-11-27 16:38:25 +0100

    binary install of gazebo to properly work

--- installers/1_gazebo.sh
+++ installers/1_gazebo.sh
@@ -29,4 +29,15 @@ popd
 rosdep install -r --from-paths src/gazebo -i -y --rosdistro ${ARENA_ROS_VERSION} \
   || echo 'rosdep failed to install all dependencies'
 
+#install binaries for simulator and ros_gz_sim
+sudo apt-get install curl lsb-release gnupg
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+sudo apt-get install gz-harmonic
+
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt install ros-jazzy-ros-gz
+
 export GZ_VERSION=${GAZEBO_VERSION}
