edf1a899a41ed4fb2d3026d6a861fbf293e83476
shibuina <anhddhe180559@fpt.edu.vn>
2024-12-12 16:35:52 +0700

    update_install_script

--- installers/2_isaac.sh
+++ installers/2_isaac.sh
@@ -11,6 +11,8 @@ fi
 
 echo "nvidia-driver was installed"
 
+python -m pip install empy==3.3.2 lark catkin_pkg pyyaml
+
 #Optional choice: install a CUDA-enabled PyTorch 2.4.0 build based on the CUDA version available on your system
 python -m pip install torch==2.4.0 --index-url https://download.pytorch.org/whl/cu121
 
@@ -23,10 +25,38 @@ python -m pip install typeguard
 #Install isaac sim
 echo "Downloading Isaac sim ..."
 python -m pip install isaacsim==4.2.0.2 --extra-index-url https://pypi.nvidia.com
-
 #Install Isaac sim - python package
 echo "Downloading Isaac sim - python package ..."
 python -m pip install isaacsim-extscache-physics==4.2.0.2 isaacsim-extscache-kit==4.2.0.2 isaacsim-extscache-kit-sdk==4.2.0.2 --extra-index-url https://pypi.nvidia.com
 python -m pip install isaacsim-kernel isaacsim-app isaacsim-asset isaacsim-benchmark isaacsim-code-editor isaacsim-core isaacsim-cortex isaacsim-example isaacsim-gui isaacsim-replicator isaacsim-rl isaacsim-robot isaacsim-robot-motion isaacsim-robot-setup isaacsim-ros1 isaacsim-ros2 isaacsim-sensor isaacsim-storage isaacsim-template isaacsim-test isaacsim-utils --extra-index-url https://pypi.nvidia.com
 
-echo "Completed download Isaac sim"
+touch ~/.ros/fastdds.xml
+echo '<?xml version="1.0" encoding="UTF-8" ?>
+
+<license>Copyright (c) 2022-2024, NVIDIA CORPORATION.  All rights reserved.
+NVIDIA CORPORATION and its licensors retain all intellectual property
+and proprietary rights in and to this software, related documentation
+and any modifications thereto.  Any use, reproduction, disclosure or
+distribution of this software and related documentation without an express
+license agreement from NVIDIA CORPORATION is strictly prohibited.</license>
+
+
+<profiles xmlns="http://www.eprosima.com/XMLSchemas/fastRTPS_Profiles" >
+    <transport_descriptors>
+        <transport_descriptor>
+            <transport_id>UdpTransport</transport_id>
+            <type>UDPv4</type>
+        </transport_descriptor>
+    </transport_descriptors>
+
+    <participant profile_name="udp_transport_profile" is_default_profile="true">
+        <rtps>
+            <userTransports>
+                <transport_id>UdpTransport</transport_id>
+            </userTransports>
+            <useBuiltinTransports>false</useBuiltinTransports>
+        </rtps>
+    </participant>
+</profiles>' > ~/.ros/fastdds.xml
+
+echo "Completed download Isaac sim" 
--- task_generator/task_generator/simulators/isaac_simulator.py
+++ task_generator/task_generator/simulators/isaac_simulator.py
@@ -1,5 +1,42 @@
 from task_generator.simulators import BaseSimulator
+# Use the isaacsim to import SimulationApp
+from isaacsim import SimulationApp
 
+# Setting the config for simulation and make an simulation.
+CONFIG = {"renderer": "RayTracedLighting", "headless": False}
+simulation_app = SimulationApp(CONFIG)
+
+# Import dependencies.
+import carb
+import math
+import omni
+import omni.graph.core as og
+import usdrt.Sdf
+import numpy as np
+import matplotlib.pyplot as plt
+import yaml
+from omni.isaac.core import SimulationContext
+from omni.isaac.core.utils.rotations import quat_to_euler_angles
+from omni.isaac.core.utils import extensions, stage, nucleus
+from omni.isaac.nucleus import get_assets_root_path
+from omni.kit.viewport.utility import get_active_viewport
+from omni.isaac.core.utils.extensions import get_extension_path_from_name
+from omni.isaac.core.utils.prims import delete_prim,get_prim_at_path,set_prim_attribute_value,get_prim_attribute_value,get_prim_attribute_names
+from omni.isaac.core.world import World
+from omni.importer.urdf import _urdf
+from omni.isaac.sensor import Camera, LidarRtx
+from omni.isaac.range_sensor import _range_sensor
+import omni.replicator.core as rep
+import omni.syntheticdata._syntheticdata as sd
+import omni.isaac.core.utils.numpy.rotations as rot_utils
+from pxr import Gf, Usd, UsdGeom
+import omni.kit.commands as commands
+import numpy as np
+import rclpy
+from rclpy.node import Node
+from isaacsim_msgs.msg import Euler, Quat, Env, Values
+from isaacsim_msgs.srv import ImportUsd, ImportUrdf, UrdfToUsd, DeletePrim, GetPrimAttributes, MovePrim, ImportYaml, ScalePrim, SpawnWall
+from sensor_msgs.msg import JointState
 
 class IsaacSimulator(BaseSimulator):
     def __init__(self, namespace):
