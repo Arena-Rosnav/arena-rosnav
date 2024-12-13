0bacfc0a4ea1332cc5c909beed68064661fb2915
tuananhroman <anh.maxiking@hotmail.de>
2024-12-13 20:16:39 +0100

    simulation state container to agent state container

--- utils/misc/rl_utils/rl_utils/state_container/container.py
+++ utils/misc/rl_utils/rl_utils/state_container/container.py
@@ -1,7 +1,13 @@
 from dataclasses import dataclass
-from .states import RobotState, TaskState
+
+from rosnav_rl.utils.agent_state import (
+    AgentStateContainer,
+    ActionSpaceState,
+    ObservationSpaceState,
+)
 
 from .distributor import StateDistributor
+from .states import RobotState, TaskState
 
 
 @dataclass(frozen=False)
@@ -11,3 +17,28 @@ class SimulationStateContainer:
 
     def distribute(self):
         StateDistributor(self).distribute()
+
+    def to_agent_state_container(self) -> AgentStateContainer:
+        return AgentStateContainer(
+            action_space=ActionSpaceState(
+                actions=self.robot.action_state.actions,
+                is_discrete=self.robot.action_state.is_discrete,
+                is_holonomic=self.robot.action_state.is_holonomic,
+            ),
+            observation_space=ObservationSpaceState(
+                laser_max_range=self.robot.laser_state.laser_max_range,
+                laser_num_beams=self.robot.laser_state.laser_num_beams,
+                ped_num_types=self.task.semantic_state.num_ped_types,
+                ped_min_speed_x=self.task.semantic_state.ped_min_speed_x,
+                ped_max_speed_x=self.task.semantic_state.ped_max_speed_x,
+                ped_min_speed_y=self.task.semantic_state.ped_min_speed_y,
+                ped_max_speed_y=self.task.semantic_state.ped_max_speed_y,
+                ped_social_state_num=self.task.semantic_state.social_state_num,
+                min_linear_vel=self.robot.action_state.velocity_state.min_linear_vel,
+                max_linear_vel=self.robot.action_state.velocity_state.max_linear_vel,
+                min_translational_vel=self.robot.action_state.velocity_state.min_translational_vel,
+                max_translational_vel=self.robot.action_state.velocity_state.max_translational_vel,
+                min_angular_vel=self.robot.action_state.velocity_state.min_angular_vel,
+                max_angular_vel=self.robot.action_state.velocity_state.max_angular_vel,
+            ),
+        )
