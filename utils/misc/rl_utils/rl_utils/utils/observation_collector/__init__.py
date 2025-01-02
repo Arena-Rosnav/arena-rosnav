3a589a7f069765b84a85c79ff067fcf361c1b3dd
tuananhroman <anh.maxiking@hotmail.de>
2025-01-02 02:47:16 +0100

    remove observation collector

--- utils/misc/rl_utils/rl_utils/utils/observation_collector/__init__.py
+++
@@ -1,7 +0,0 @@
-from typing import Callable, Dict, Generic, TypeVar, Union
-
-from .collectors import *
-from .generators import *
-from .observation_manager import ObservationManager
-from .static import *
-from .traversal import get_required_observation_units
--- utils/misc/rl_utils/rl_utils/utils/observation_collector/collectors/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_collector import BaseUnit, ObservationCollectorUnit
-from .image import *
-from .pedestrian import *
-from .base import *
-from .unity import *
