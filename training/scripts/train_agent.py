1db7fdc6f665f3f01bacc9e0a9b121ad814c0e07
tuananhroman <anh.maxiking@hotmail.de>
2025-01-24 21:12:41 +0100

    remove redundant comments

--- training/scripts/train_agent.py
+++ training/scripts/train_agent.py
@@ -4,12 +4,6 @@ from rl_utils.trainer.sb3_trainer import StableBaselines3Trainer
 from tools.argsparser import parse_training_args
 from tools.config import load_training_config
 
-# enhance configuration files with field definition
-# rosparam adjustments
-# loading method agent level and algorithm level
-# restructure folders based on framework -> algorithm
-# dynamic parameter for algo and task
-
 
 def main():
     args, _ = parse_training_args()
