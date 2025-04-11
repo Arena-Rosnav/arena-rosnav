77a9e7df54279f3867bee359efe11d58793e8270
Vova Shcherbyna <dev@voshch.dev>
2025-04-11 16:07:38 +0200

    tweak

--- tools/source.bash
+++ tools/source.bash
@@ -13,7 +13,7 @@ if [ -z ${ARENA_SOURCED+x} ] ; then
     export FASTRTPS_DEFAULT_PROFILES_FILE=~/.ros/fastdds.xml
     export ROS_DOMAIN_ID=1
     export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ros/humble/lib/
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/ros/${ARENA_ROS_DISTRO}/lib/"
     export INSTALLED=$ARENA_WS_DIR/src/arena/arena-rosnav/.installed
 
     pushd src/arena/arena-rosnav
