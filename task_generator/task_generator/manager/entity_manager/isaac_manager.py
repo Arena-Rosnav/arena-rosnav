d5373beab0fa2fc57f77218dd51e2f2edda72c7e
Vova Shcherbyna <dev@voshch.dev>
2025-05-15 17:09:34 +0200

    isaacsim load random peds

--- task_generator/task_generator/manager/entity_manager/isaac_manager.py
+++ task_generator/task_generator/manager/entity_manager/isaac_manager.py
@@ -3,13 +3,13 @@ import typing
 from arena_rclpy_mixins.shared import Namespace
 from isaacsim_msgs.msg import Person
 from isaacsim_msgs.srv import Pedestrian
-
 from task_generator.manager.entity_manager.dummy_manager import \
     DummyEntityManager
 from task_generator.manager.entity_manager.utils import (KnownObstacles,
                                                          ObstacleLayer)
 from task_generator.shared import DynamicObstacle
 from task_generator.simulators import BaseSimulator
+from task_generator.simulators.isaac_simulator import IsaacSimulator
 
 
 class IsaacEntityManager(DummyEntityManager):
@@ -17,7 +17,10 @@ class IsaacEntityManager(DummyEntityManager):
     _known_obstacles: KnownObstacles
 
     def __init__(self, namespace: Namespace, simulator: BaseSimulator):
+        if not isinstance(simulator, IsaacSimulator):
+            raise ValueError("IsaacEntityManager only works with IsaacSimulator")
         super().__init__(namespace, simulator)
+        self._simulator = typing.cast(IsaacSimulator, self._simulator)
         self._known_obstacles = KnownObstacles()
 
     def spawn_dynamic_obstacles(
@@ -32,13 +35,13 @@ class IsaacEntityManager(DummyEntityManager):
                     name=obstacle.name,
                     obstacle=obstacle
                 )
-                self._simulator.client['spawn_pedestrian_client'].call_async(
+                self._simulator.services.import_pedestrians.client.call(
                     Pedestrian.Request(
                         people=[Person(
                             stage_prefix=obstacle.name,
-                            character_name=obstacle.model._name,
+                            character_name=obstacle.model.name,
                             initial_pose=[obstacle.position.x, obstacle.position.y, .1],
-                            goal_pose=[obstacle.waypoints[-1][0], obstacle.waypoints[-1][1], 0.0],
+                            goal_pose=[obstacle.waypoints[-1].x, obstacle.waypoints[-1].y, 0.0],
                             orientation=obstacle.position.orientation,
                             controller_stats=False,
                             velocity=0.407306046952996
