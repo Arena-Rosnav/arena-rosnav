e2d30581151e5b19f9df3b2f924b747be367cdb7
Vova Shcherbyna <dev@voshch.dev>
2025-07-10 21:00:51 +0200

    final touch

--- arena_bringup/launch/arena.launch.py
+++ arena_bringup/launch/arena.launch.py
@@ -59,7 +59,7 @@ def generate_launch_description():
     human = LaunchArgument(
         name='human',
         description='human simulator to use',
-        default_value=PythonExpression([str({"gazebo": "hunav", "isaac": "isaac"}), '.get("', sim.substitution, '", "dummy")']),
+        default_value=PythonExpression([str({"gazebo": "hunav", "isaac": "hunav"}), '.get("', sim.substitution, '", "dummy")']),
     )
     complexity = LaunchArgument(
         name='complexity',
